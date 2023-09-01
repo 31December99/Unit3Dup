00f6bc94df14d7c7e16f4d7749cf45aff0a865d0
midnight <jw79985@gmail.com>
2023-09-01 16:15:17 +0200

    Updated mytorr attributes

--- pvtTorrent.py
+++ pvtTorrent.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3.9
+import os
 from pathlib import Path, PurePath
+from decouple import config
 import torf
+
+import utitlity
+
+ITT_PASS_KEY = config('ITT_PASS_KEY')
+ITT_API_TOKEN = config('ITT_API_TOKEN')
 
 
 class Mytorrent:
@@ -8,14 +15,18 @@ class Mytorrent:
     def __init__(self, contents: str):
         self.contents = Path(contents)
         self.file_name = PurePath(self.contents).name if Path.is_dir(self.contents) else Path(contents).name
+        self.title, self.extension = os.path.splitext(self.file_name)
         self.mytorr = torf.Torrent(path=self.file_name)
+        self.mytorr.announce_list = [f"https://itatorrents.xyz/announce/{ITT_PASS_KEY}/"]
+        self.mytorr.comment = "ciao"
+        self.mytorr.name = self.file_name
         self.mytorr.generate()
         self.mytorr.created_by = "bITT"
         self.mytorr.private = True
         self.mytorr.segments = 16 * 1024 * 1024  # 16MB
 
-    def write(self, torrent_filename: str):
-        self.mytorr.write(f"{torrent_filename}.torrent")
+    def write(self):
+        self.mytorr.write(f"{self.file_name}.torrent")
 
     @property
     def comment(self):
@@ -27,11 +38,7 @@ class Mytorrent:
 
     @property
     def name(self):
-        return self.mytorr.name
-
-    @name.setter
-    def name(self, value):
-        self.mytorr.name = value
+        return self.title
 
     @property
     def info_hash(self):
