c22de718b5b0628ebe669f48d7df667655323c2e
midnight <jw79985@gmail.com>
2023-09-01 20:06:17 +0200

    Added filter type_c

--- utitlity.py
+++ utitlity.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.9
 
+
 class Manage_titles:
     marks = [',', ';', ':', '!', '?', '"', '(', ')', '[', ']', '{', '}', '/', '\\', '&', '*',
              '$', '%', '#', '@', '_']
@@ -11,3 +12,34 @@ class Manage_titles:
             name = name.replace(punct, '')
         return name
 
+    @staticmethod
+    def filterType(file_name: str) -> int:
+        word_list = file_name.lower().strip().split(" ")
+        lista = [
+            "fulldisc",
+            "remux",
+            "encode",
+            "web-dl",
+            "webrip",
+            "hdtv",
+            "flac",
+            "alac",
+            "ac3",
+            "mp3",
+            "mac",
+            "windows",
+            "cinema",
+            "altro",
+            "pdf",
+            "nintendo",
+            "ps4",
+            "epub",
+            "mp4",
+            "pack",
+            "avi",
+        ]
+
+        for word in word_list:
+            if word in lista:
+                # Tracker type starts from zero
+                return lista.index(word) + 1
