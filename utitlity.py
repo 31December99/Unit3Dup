d1ddb35630bf93107467983d381ee7b515728d52
midnight <jw79985@gmail.com>
2023-09-03 20:36:24 +0200

    Added code for console prints

--- utitlity.py
+++ utitlity.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3.9
+from datetime import datetime
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Console:
+
+    @staticmethod
+    def print(message: str, level: int):
+        now = datetime.now()
+        date_now = datetime.today().strftime('%d-%m-%Y')
+        time_now = now.strftime("%H:%M:%S")
+        if level == 1:
+            print(f"<{date_now} {time_now}>{bcolors.FAIL}{message}{bcolors.ENDC}")
+        if level == 2:
+            print(f"<{date_now} {time_now}>{bcolors.OKGREEN}{message}{bcolors.ENDC}")
+        if level == 3:
+            print(f"<{date_now} {time_now}>{bcolors.WARNING}{message}{bcolors.ENDC}")
 
 
 class Manage_titles:
