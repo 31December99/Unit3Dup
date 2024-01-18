a2ad42b5a0b456a7eae5933b4365ce19ce589fbe
midnight <jw79985@gmail.com>
2024-01-18 18:15:14 +0100

    Removed colors

--- utitlity.py
+++ utitlity.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 
 class Manage_titles:
     # todo: rimvuoere '.' senza rimuoverli da DD5.1 H.264 ecc
