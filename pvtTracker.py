2bbc6c0177bb5d860221acff78e02a896d40f66d
midnight <jw79985@gmail.com>
2024-01-18 18:15:36 +0100

    Removed colors

--- pvtTracker.py
+++ pvtTracker.py
@@ -2,19 +2,6 @@
 from datetime import datetime
 import requests
 
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
-
 class Myhttp:
 
     def __init__(self, base_url: str, api_token: str, pass_key: str):
