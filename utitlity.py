d0eddc55d5fbbdf50f017353b930f31a68059e3f
midnight <jw79985@gmail.com>
2024-01-18 18:14:25 +0100

    removed shebang and console

--- utitlity.py
+++ utitlity.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3.9
-import re
-from datetime import datetime
-
-
+# -*- coding: utf-8 -*-
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -13,21 +9,6 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-
-
-class Console:
-
-    @staticmethod
-    def print(message: str, level: int):
-        now = datetime.now()
-        date_now = datetime.today().strftime('%d-%m-%Y')
-        time_now = now.strftime("%H:%M:%S")
-        if level == 1:
-            print(f"<{date_now} {time_now}>{bcolors.FAIL}{message}{bcolors.ENDC}")
-        if level == 2:
-            print(f"<{date_now} {time_now}>{bcolors.OKGREEN}{message}{bcolors.ENDC}")
-        if level == 3:
-            print(f"<{date_now} {time_now}>{bcolors.WARNING}{message}{bcolors.ENDC}")
 
 
 class Manage_titles:
