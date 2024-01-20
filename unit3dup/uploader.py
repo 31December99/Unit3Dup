edb1839d382c8493417835730fcc3a959a636eb5
midnight <jw79985@gmail.com>
2024-01-20 19:54:18 +0100

    minor fix

--- unit3dup/uploader.py
+++ unit3dup/uploader.py
@@ -29,8 +29,8 @@ class Bot:
             logging.info("il file .env non è stato configurato oppure i nomi delle variabili sono errate.")
             return
         if not data:
-            print("[BOT] Non riconosco il nome del tracker che hai impostato nel file .env di configurazione")
-            print("[BOT] Di seguito i nomi disponibili per il tuo tracker:")
+            print("[BOT] Non riconosco il nome del tracker che hai impostato nel file .env di configurazione\n"
+                  "[BOT] Di seguito i nomi disponibili per il tuo tracker:")
             for tracker in trackers:
                 print(f"[BOT] <{tracker}>")
             print("[BOT] Verifica ora il tuo file .env")
@@ -40,15 +40,15 @@ class Bot:
             print("Devi scegliere tra --movie e --serie. Esempio 'start.py -serie' seguito dal percorso completo")
             return
 
-        self.tracker_values = data()
         print(f"\n[TRACKER]..............  {BASE_URL}")
-        self.tracker = pvtTracker.ITT(base_url=BASE_URL, api_token=API_TOKEN,
-                                      pass_key=PASS_KEY)
+        self.tracker_values = data()
+
+        # // Options
         if args.serie:
             self.mytmdb = search.TvShow('Serie')
             self.content = Contents.Args(args.serie)
             self.metainfo = self.content.folder()
-            self.tracker.data['name'] = utitlity.Manage_titles.clean(self.content.base_name)
+            self.name = utitlity.Manage_titles.clean(self.content.base_name)
             self.myguess = myTMDB.Myguessit(self.content.file_name)
             self.result = self.mytmdb.start(str(self.myguess.guessit_title))
             self.category = self.tracker_values.category['serie_tv']
@@ -57,22 +57,23 @@ class Bot:
             self.mytmdb = search.TvShow('Movie')
             self.content = Contents.Args(args.movie)
             self.metainfo = self.content.file()
-            self.tracker.data['name'] = utitlity.Manage_titles.clean(self.content.tracker_file_name)
+            self.name = utitlity.Manage_titles.clean(self.content.tracker_file_name)
             self.myguess = myTMDB.Myguessit(self.content.file_name)
             self.result = self.mytmdb.start(str(self.myguess.guessit_title))
             self.category = self.tracker_values.category['movie']
 
-        self.tracker.data['tmdb'] = self.result.video_id
-        self.tracker.data['keywords'] = self.result.keywords
-
-        self.mytorrent = pvtTorrent.Mytorrent(contents=self.content, meta=self.metainfo)
+        # // Video data
         self.video = pvtVideo.Video(fileName=str(os.path.join(self.content.path, self.content.file_name)))
-        self.torrent = self.mytorrent.write
         self.standard = self.video.standard
         self.media_info = self.video.mediainfo
         self.descrizione = self.video.description
         self.freelech = self.tracker_values.get_freelech(self.video.size)
 
+        # // Tracker data
+        self.tracker = pvtTracker.ITT(base_url=BASE_URL, api_token=API_TOKEN, pass_key=PASS_KEY)
+        self.tracker.data['name'] = self.name
+        self.tracker.data['tmdb'] = self.result.video_id
+        self.tracker.data['keywords'] = self.result.keywords
         self.tracker.data['category_id'] = self.category
         self.tracker.data['resolution_id'] = self.tracker_values.filterResolution(self.content.file_name)
         self.tracker.data['free'] = self.freelech
@@ -83,8 +84,14 @@ class Bot:
         self.tracker.data['season_number'] = int(self.myguess.guessit_season)
         self.tracker.data['episode_number'] = int(self.myguess.guessit_season)
 
+        # // Torrent
+        self.mytorrent = pvtTorrent.Mytorrent(contents=self.content, meta=self.metainfo)
+        self.torrent = self.mytorrent.write
+
+        # // Send data
         tracker_response = self.tracker.upload_t(data=self.tracker.data, file_name=os.path.join(self.content.path,
                                                                                                 self.mytorrent.read()))
+        # // Seeding
         if tracker_response.status_code == 200:
             tracker_response_body = json.loads(tracker_response.text)
             logging.info(tracker_response_body['message'])
