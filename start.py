207f92c3f7146440b381cf4165579bb671ec30bd
midnight <jw79985@gmail.com>
2024-01-20 18:39:16 +0100

    Added argparse

--- start.py
+++ start.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-
-
+import argparse
 from decouple import config
 from unit3dup.uploader import Bot, trackers
 
 TRACKER_NAME = config('TRACK_NAME')
 
-
 if __name__ == "__main__":
-    bot = Bot(trackers.get(TRACKER_NAME))
+    parser = argparse.ArgumentParser(description='Commands', add_help=False)
+    parser.add_argument('-serie', '--serie', nargs=1, type=str, help='Serie')
+    parser.add_argument('-movie', '--movie', nargs=1, type=str, help='Movie')
+    args = parser.parse_args()
+
+    bot = Bot(trackers.get(TRACKER_NAME), args=args)
