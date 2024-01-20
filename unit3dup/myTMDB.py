b91e9ef04c40dd637abcd3b14f32b97236cb24d4
midnight <jw79985@gmail.com>
2024-01-20 20:55:29 +0100

    removed guessit class

--- unit3dup/myTMDB.py
+++ unit3dup/myTMDB.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import tmdbv3api.exceptions
-import guessit
 from unidecode import unidecode
 from decouple import config
 from tmdbv3api import TMDb, Movie, TV
@@ -9,75 +8,6 @@ from unit3dup.results import Results
 from unit3dup.utitlity import Manage_titles
 
 TMDB_APIKEY = config('TMDB_APIKEY')
-
-
-class Myguessit:
-
-    def __init__(self, filename: str):
-        self.guessit = guessit.guessit(filename)
-        self.filename = filename
-
-    @property
-    def guessit_title(self):
-        """
-        Estrae la stringa con il titolo dal nome del file film_title o title(serie ?)
-        :return:
-        """
-        #
-        # Se fallisce guessit ad esempio in questo caso :
-        # titolo : 1923 ( nessun altra informazione nel titolo)
-        # MatchesDict([('year', 1923), ('type', 'movie')])
-        # dove non trova ne title e film_title e erroneamente lo credo un movie..
-        # bypass guessit e ritorna filename alla ricerca di tmdb
-        return self.guessit.get('film_title', self.guessit.get('title', self.filename))
-
-    @property
-    def guessit_alternative(self):
-        """
-        Estrae la stringa con il titolo dal nome del file film_title o title(serie ?)
-        :return:
-        """
-        return self.guessit.get('alternative_title', self.guessit.get('title', self.filename))
-
-    @property
-    def guessit_year(self):
-        """
-        Estrae l'anno di pubblicazione dal titolo
-        :return:
-        """
-        return self.guessit['year'] if 'year' in self.guessit else None
-
-    @property
-    def guessit_episode(self):
-        """
-        Estrae il numero di episodio dal titolo
-        :return:
-        """
-        return self.guessit['episode'] if 'episode' in self.guessit else 0
-
-    @property
-    def guessit_season(self):
-        """
-        Estrae il numero di stagione dal titolo
-        :return:
-        """
-        return self.guessit['season'] if 'season' in self.guessit else 0
-
-    @property
-    def type(self):
-        """
-        Determina se è una serie verificando la presenza di un numero di stagione
-        :return:
-        """
-        return self.guessit['type'] if 'type' in self.guessit else None
-
-    @property
-    def is_movie(self):
-        """
-        Determina se è un movie varificando la presenza diu numero di stagione
-        :return:
-        """
-        return True if not self.season else False
 
 
 class MyTmdb:
