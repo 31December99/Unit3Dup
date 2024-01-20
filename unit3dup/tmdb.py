f3fe80fad447ad72846759abe98ec9287c5e1afb
midnight <jw79985@gmail.com>
2024-01-20 23:52:42 +0100

    fixed bug when printing the search name

--- unit3dup/tmdb.py
+++ unit3dup/tmdb.py
@@ -33,12 +33,12 @@ class MyTmdb:
             self.tmdb = self.__mv_tmdb
 
     def search(self, ext_title: str):
-        print(f"\n[TMDB Search]..........  {self.ext_title}")
         self.ext_title = ext_title.lower()
         self.ext_title = self.ext_title.replace('-', ' ')
         self.ext_title = self.ext_title.replace('–', ' ')
         self.ext_title = self.ext_title.replace('’', ' ')
         self.ext_title = self.ext_title.replace('\'', ' ')
+        print(f"\n[TMDB Search]..........  {self.ext_title}")
 
         self.__requests()
         result = self.__search_titles()
