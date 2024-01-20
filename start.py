467dd7b1e38c450e0518deafbab65bcc5fe983e3
midnight <jw79985@gmail.com>
2024-01-20 18:06:16 +0100

    Fixed importing lib

--- start.py
+++ start.py
@@ -2,10 +2,10 @@
 
 
 from decouple import config
-from unit3d.bot import Bot, trackers
+from unit3d.uploader import Bot, trackers
 
 TRACKER_NAME = config('TRACK_NAME')
 
 
 if __name__ == "__main__":
-    bot = Bot(trackers.get(TRACKER_NAME.lower()))
+    bot = Bot(trackers.get(TRACKER_NAME))
