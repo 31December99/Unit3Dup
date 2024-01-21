95766fe102e49431746697d436ce8da3aa7ae830
midnight <jw79985@gmail.com>
2024-01-21 21:27:28 +0100

    Added a new command to choose the tracker by name

--- start.py
+++ start.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import argparse
-from unit3dup.uploader import Bot
+from unit3dup.uploader import UploadBot
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Commands', add_help=False)
-    parser.add_argument('-serie', '--serie', nargs=1, type=str, help='Serie')
-    parser.add_argument('-movie', '--movie', nargs=1, type=str, help='Movie')
+    parser.add_argument('-s', '--serie', nargs=1, type=str, help='Serie')
+    parser.add_argument('-m', '--movie', nargs=1, type=str, help='Movie')
+    parser.add_argument('-t', '--tracker', nargs=1, type=str, help='Tracker Name')
     args = parser.parse_args()
 
-    bot = Bot(args=args)
-
+    bot = UploadBot(args=args)
