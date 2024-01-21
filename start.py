13972b6a2a9aea66a6dd848379e1e20ab036bb45
midnight <jw79985@gmail.com>
2024-01-21 16:42:58 +0100

    Replaced the reading of tracker config

--- start.py
+++ start.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 import argparse
-from decouple import config
-from unit3dup.uploader import Bot, trackers
-
-TRACKER_NAME = config('TRACK_NAME')
+from unit3dup.uploader import Bot
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Commands', add_help=False)
@@ -11,4 +8,5 @@ if __name__ == "__main__":
     parser.add_argument('-movie', '--movie', nargs=1, type=str, help='Movie')
     args = parser.parse_args()
 
-    bot = Bot(trackers.get(TRACKER_NAME), args=args)
+    bot = Bot(args=args)
+
