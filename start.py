470f5c86a6c4ea3e39599218712b02e366fb95eb
Parzival <jw79985@gmail.com>
2024-07-07 22:56:38 +0200

    Add colored messages

--- start.py
+++ start.py
@@ -2,6 +2,9 @@
 import argparse
 from unit3dup.uploader import UploadBot
 from unit3dup.contents import Cli
+from rich.console import Console
+
+console = Console(log_path=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Commands', add_help=False)
@@ -9,11 +12,13 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--tracker', nargs=1, type=str, help='Tracker Name')
     args = parser.parse_args()
 
+    console.rule(f"\n[bold blue] Unit3D uploader", style="#ea00d9")
     if args.upload:
         user_input = Cli(args=args, tracker=args.tracker)
-
         if user_input:
             bot = UploadBot(user_input.content)
+            console.log(f"\n[TORRENT NAME] {bot.name}")
+            console.log(f"[SIZE]         {user_input.size}")
             if user_input.content.category == user_input.serie:
                 data = bot.serie_data()
             else:
