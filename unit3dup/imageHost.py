79f59cbc17c6f4392407a1f449282e0c5fe09b31
Parzival <jw79985@gmail.com>
2024-07-07 22:55:57 +0200

    Add colored messages

--- unit3dup/imageHost.py
+++ unit3dup/imageHost.py
@@ -3,9 +3,11 @@ import base64
 import os
 import requests
 from decouple import Config, RepositoryEnv
+from rich.console import Console
 
 config_load = Config(RepositoryEnv('service.env'))
 IMGBB_KEY = config_load('IMGBB_KEY')
+console = Console()
 
 
 class ImgHost:
@@ -40,5 +42,4 @@ class ImgBB(ImgHost):
             'image': (None, self.image),
         }
         response = requests.post('https://api.imgbb.com/1/upload', params=params, files=files)
-        print(f"[IMG]..........  {response.json()['data']['url_viewer']}")
         return response.json()
