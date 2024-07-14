e823cfb38851fd24db20be3f704e57926faf3a48
Parzival <jw79985@gmail.com>
2024-07-14 15:31:45 +0200

    Removed screenshot file saving

--- unit3dup/imageHost.py
+++ unit3dup/imageHost.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import base64
-import os
 import requests
 from decouple import Config, RepositoryEnv
 from rich.console import Console
@@ -10,27 +9,10 @@ IMGBB_KEY = config_load('IMGBB_KEY')
 console = Console()
 
 
-class ImgHost:
+class ImgBB:
 
-    def __init__(self, filename: str):
-        self.filename = os.path.join("images", filename)
-        self.params = {}
-        self.file = {}
-
-    @property
-    def image(self) -> base64:
-        if os.path.exists(self.filename):
-            with open(self.filename, 'rb') as open_image:
-                image_data = open_image.read()
-                image_encode = base64.b64encode(image_data)
-            os.remove(self.filename)
-            return image_encode
-
-    def upload(self):
-        pass
-
-
-class ImgBB(ImgHost):
+    def __init__(self, image: bytes):
+        self.image = base64.b64encode(image)
 
     @property
     def upload(self):
