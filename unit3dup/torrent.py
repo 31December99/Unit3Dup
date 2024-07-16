a2cf065471d71b58a1813aebb52bb9a0037bc9b3
Parzival <jw79985@gmail.com>
2024-07-16 23:01:16 +0200

    add search filename

--- unit3dup/torrent.py
+++ unit3dup/torrent.py
@@ -115,6 +115,15 @@ class Torrent:
         if tracker_data:
             self.print_normal(data=tracker_data['data'])
 
+    def get_by_filename(self, file_name: str):
+        tracker = pvtTracker.Unit3d(
+            base_url=self.BASE_URL, api_token=self.API_TOKEN, pass_key=self.PASS_KEY
+        )
+        tracker_data = tracker.get_filename(file_name=file_name[0], perPage=25)
+        console.log(f"Filename torrents.. Filter by the torrent's filename.. '{file_name[0].upper()}'")
+        if tracker_data:
+            self.print_normal(data=tracker_data['data'])
+
     def get_by_tmdb_id(self, tmdb_id: int):
         tracker = pvtTracker.Unit3d(
             base_url=self.BASE_URL, api_token=self.API_TOKEN, pass_key=self.PASS_KEY
