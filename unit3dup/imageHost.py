62a146c12eeaacab98ca370feb3710d2f294401c
Parzival <jw79985@gmail.com>
2024-07-19 18:15:22 +0200

    Remove unused var in try/except

--- unit3dup/imageHost.py
+++ unit3dup/imageHost.py
@@ -29,7 +29,7 @@ class ImgBB:
             response = requests.post('https://api.imgbb.com/1/upload', params=params, files=files)
             response.raise_for_status()
             return response.json()
-        except requests.exceptions.HTTPError as http_err:
+        except requests.exceptions.HTTPError:
             error = response.json()
             console.log(f"[Report IMGBB] Screenshot {error['error']['message']} - Renew your API KEY")
             sys.exit()
