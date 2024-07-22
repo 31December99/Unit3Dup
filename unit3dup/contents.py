49559035e6b5bf1d269755ccf2eae0195e56b09e
Parzival <jw79985@gmail.com>
2024-07-22 22:40:14 +0200

    Fix typing

--- unit3dup/contents.py
+++ unit3dup/contents.py
@@ -68,7 +68,7 @@ class Cli:
         self.folder = None
         self.file_name = None
 
-    def get_data(self) -> [userinput.Contents, bool]:
+    def get_data(self) -> userinput.Contents | bool:
         """
         Create an userinput object with movie or series attributes for the torrent.
         Verify if name is part of torrent pack folder. If there is no episode it's a pack
@@ -122,7 +122,7 @@ class Cli:
         self.meta_info = json.dumps(self.meta_info_list, indent=4)
         return True
 
-    def create_movies_path(self, file: str) -> Union[File, None]:
+    def create_movies_path(self, file: str) -> File | None:
         """
         Determines if it is a movie or a series. Excludes any episode files.
         """
@@ -133,7 +133,7 @@ class Cli:
         else:
             return None
 
-    def create_series_path(self, subdir: str) -> [Folder | None]:
+    def create_series_path(self, subdir: str) -> Folder | None:
 
         """
         Determines whether the folder contains an Sx tag
@@ -175,12 +175,13 @@ class Cli:
 
         movies_path: list = []
         series_path: list = []
-        sub_dirs_found: bool = False
+
+        # Start analyzing the path
         for path, subdirs, files in os.walk(self.path):
             if path == self.path:
                 movies_path = [os.path.join(self.path, file) for file in files if self.filter_ext(file)]
             if subdirs:
-                sub_dirs_found = True
+                # Maximum level of subfolder depth = 1
                 if self.depth_walker(path) < 1:
                     series_path = [os.path.join(self.path, subdir) for subdir in subdirs]
 
