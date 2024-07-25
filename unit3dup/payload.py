ef575a67a2f52df6cd08202994c73ef40f0337a0
Parzival <jw79985@gmail.com>
2024-07-25 23:24:02 +0200

    Fix name

--- unit3dup/payload.py
+++ unit3dup/payload.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from unit3dup import results, utitlity, title
+from unit3dup import results, utility, title
+
 
 class Data:
 
-    def __init__(self, metainfo: str, name: str, file_name: str, result: results, category: int, standard: int, mediainfo: str,
+    def __init__(self, metainfo: str, name: str, file_name: str, result: results, category: int, standard: int,
+                 mediainfo: str,
                  description: str, freelech: int):
-
         self.metainfo = metainfo
-        self.name = utitlity.Manage_titles.clean(name)
+        self.name = utility.Manage_titles.clean(name)
         self.file_name = file_name
         self.myguess = title.Guessit(file_name)
         self.result = result
@@ -18,8 +19,7 @@ class Data:
         self.freelech = freelech
 
     @classmethod
-    def create_instance(cls, metainfo: str, name: str, file_name: str, result: results, category: int, standard: int, mediainfo: str,
+    def create_instance(cls, metainfo: str, name: str, file_name: str, result: results, category: int, standard: int,
+                        mediainfo: str,
                         description: str, freelech: int):
         return cls(metainfo, name, file_name, result, category, standard, mediainfo, description, freelech)
-
-
