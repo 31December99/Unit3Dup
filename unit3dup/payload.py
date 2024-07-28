258885a9fbd478ea3696dc22b24ccb8526aa4faf
Parzival <jw79985@gmail.com>
2024-07-28 17:48:31 +0200

    black

--- unit3dup/payload.py
+++ unit3dup/payload.py
@@ -2,11 +2,19 @@
 from unit3dup import results, utility, title
 
 
-
 class Data:
 
-    def __init__(self, metainfo: str, name: str, file_name: str, result: results, category: int, standard: int,
-                 mediainfo: str, description: str):
+    def __init__(
+        self,
+        metainfo: str,
+        name: str,
+        file_name: str,
+        result: results,
+        category: int,
+        standard: int,
+        mediainfo: str,
+        description: str,
+    ):
         self.metainfo = metainfo
         self.name = utility.Manage_titles.clean(name)
         self.file_name = file_name
@@ -18,6 +26,24 @@ class Data:
         self.description = description
 
     @classmethod
-    def create_instance(cls, metainfo: str, name: str, file_name: str, result: results, category: int, standard: int,
-                        mediainfo: str, description: str):
-        return cls(metainfo, name, file_name, result, category, standard, mediainfo, description)
+    def create_instance(
+        cls,
+        metainfo: str,
+        name: str,
+        file_name: str,
+        result: results,
+        category: int,
+        standard: int,
+        mediainfo: str,
+        description: str,
+    ):
+        return cls(
+            metainfo,
+            name,
+            file_name,
+            result,
+            category,
+            standard,
+            mediainfo,
+            description,
+        )
