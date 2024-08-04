f39b1788e7f89609602ec48a35d9641f6e9270af
Parzival <jw79985@gmail.com>
2024-08-04 15:08:54 +0200

    Refac

--- unit3dup/media.py
+++ unit3dup/media.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+
+import requests
+
 from unit3dup.pvtTorrent import Mytorrent
-from unit3dup.uploader import UploadBot
+from unit3dup.uploader import UploadDocument, UploadVideo
 from unit3dup.contents import Contents
 from unit3dup.pvtVideo import Video
 from unit3dup.automode import Auto
@@ -33,25 +36,24 @@ class Media:
 
         # Load the json file
         self.config = config.trackers.get_tracker(tracker_name=tracker_name)
-        self.movie_category = self.config.tracker_values.category('movie')
-        self.serie_category = self.config.tracker_values.category('tvshow')
-        self.docu_category = self.config.tracker_values.category('e-book')
+        self.movie_category = self.config.tracker_values.category("movie")
+        self.serie_category = self.config.tracker_values.category("tvshow")
+        self.docu_category = self.config.tracker_values.category("e-book")
 
-    def process_contents(self, mode='man'):
-        if mode == 'man':
+    def process_contents(self, mode="man"):
+        if mode == "man":
             files = self.manual()
         else:
             files = self.auto()
 
         for item in files:
             content = self.video_files(item)
-            print(content.file_name, content.category)
             if not content:
                 continue
             self.contents.append(content)
         return self.contents
 
-    def process(self, mode='man'):
+    def process(self, mode="man"):
         contents = self.process_contents(mode=mode)
 
         response = None
@@ -71,10 +73,13 @@ class Media:
                 video_info = self.video_info(content=content)
 
                 # Send
-                response = self.unit3d(content=content, tv_show_result=tv_show_results, video_info=video_info)
+                response = Media.unit3d(
+                    content=content,
+                    tv_show_result=tv_show_results,
+                    video_info=video_info,
+                )
 
             if content.category == self.docu_category:
-
                 # Send
                 response = self.unit3d_doc(content=content)
 
@@ -87,21 +92,50 @@ class Media:
                 )
 
     def manual(self):
-        auto = Auto(path=self.path, mode='man', tracker_name=self.tracker_name)
+        auto = Auto(path=self.path, mode="man", tracker_name=self.tracker_name)
         return auto.upload()
 
     def auto(self):
         auto = Auto(path=self.path, tracker_name=self.tracker_name)
         return auto.scan()
 
+    @staticmethod
+    def unit3d(content: Contents, tv_show_result: list, video_info: pvtTracker) -> requests:
+        # Prepare for upload
+        unit3d_up = UploadVideo(content)
+
+        # Create a new payload
+        data = unit3d_up.payload(tv_show=tv_show_result, video=video_info)
+
+        # Get a new tracker instance
+        tracker = unit3d_up.tracker(data=data)
+
+        # Send the payload
+        return unit3d_up.send(tracker=tracker)
+
+    @staticmethod
+    def unit3d_doc(content: Contents) -> requests:
+
+        # Prepare for upload
+        unit3d_up = UploadDocument(content)
+
+        # Create a new payload
+        data = unit3d_up.payload()
+
+        # Get a new tracker instance
+        tracker = unit3d_up.tracker(data=data)
+
+        # Send the payload
+        return unit3d_up.send(tracker=tracker)
+
     def video_files(self, item):
         """
-            Getting ready for tracker upload
-            Return
-                  - torrent name (filename or folder name)
-                  - content category ( movie or serie)
-                  - torrent meta_info
-            """
+        Getting ready for tracker upload
+        Return
+              - torrent name (filename or folder name)
+              - content category ( movie or serie)
+              - torrent meta_info
+        """
         video_files = Files(
             path=item.torrent_path,
             tracker_name=self.tracker_name,
@@ -114,7 +148,7 @@ class Media:
         return content
 
     def db_search(self, content: Contents):
-        # Request results from the TVshow online database
+        # Request results from the video online database
         my_tmdb = TvShow(content.category)
         tv_show_result = my_tmdb.start(content.file_name)
         return tv_show_result
@@ -124,11 +158,3 @@ class Media:
             fileName=str(os.path.join(content.folder, content.file_name))
         )
         return video_info
-
-    def unit3d(self, content: Contents, tv_show_result: list, video_info: pvtTracker):
-        unit3d_up = UploadBot(content)
-        return unit3d_up.send(tv_show=tv_show_result, video=video_info)
-
-    def unit3d_doc(self, content: Contents):
-        unit3d_up = UploadBot(content)
-        return unit3d_up.send_doc()
