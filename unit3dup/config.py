b76cfc2771274085f14a839cea9e02e6d7064cec
Parzival <jw79985@gmail.com>
2024-08-11 20:26:07 +0200

    Add error handling

--- unit3dup/config.py
+++ unit3dup/config.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+
+import decouple
 from decouple import Config, RepositoryEnv
 from rich.console import Console
 from rich.text import Text
@@ -53,15 +55,15 @@ class ConfigUnit3D:
         # Get the current folder
         self.current_folder = os.path.dirname(__file__)
         self.root_folder = os.path.abspath(os.path.join(self.current_folder, ".."))
-        self.TMDB_APIKEY: str = ''
-        self.IMGBB_KEY: str = ''
-        self.FREE_IMAGE_KEY: str = ''
-        self.QBIT_USER: str = ''
-        self.QBIT_PASS: str = ''
-        self.QBIT_URL: str = ''
-        self.QBIT_PORT: str = ''
-        self.API_TOKEN: str = ''
-        self.BASE_URL: str = ''
+        self.TMDB_APIKEY: str = ""
+        self.IMGBB_KEY: str = ""
+        self.FREE_IMAGE_KEY: str = ""
+        self.QBIT_USER: str = ""
+        self.QBIT_PASS: str = ""
+        self.QBIT_URL: str = ""
+        self.QBIT_PORT: str = ""
+        self.API_TOKEN: str = ""
+        self.BASE_URL: str = ""
         self.tracker_values: dict = {}
         self.trackers = None
 
@@ -85,20 +87,28 @@ class ConfigUnit3D:
             )
             console.log(self.message)
 
-        config_load_service = Config(RepositoryEnv(service_path))
-        self.TMDB_APIKEY = config_load_service("TMDB_APIKEY")
-        self.IMGBB_KEY = config_load_service("IMGBB_KEY")
-        self.FREE_IMAGE_KEY = config_load_service("FREE_IMAGE_KEY")
-        self.QBIT_USER = config_load_service("QBIT_USER")
-        self.QBIT_PASS = config_load_service("QBIT_PASS")
-        self.QBIT_URL = config_load_service("QBIT_URL")
-        self.QBIT_PORT = config_load_service("QBIT_PORT")
+        try:
+            config_load_service = Config(RepositoryEnv(service_path))
+            self.TMDB_APIKEY = config_load_service("TMDB_APIKEY")
+            self.IMGBB_KEY = config_load_service("IMGBB_KEY")
+            self.FREE_IMAGE_KEY = config_load_service("FREE_IMAGE_KEY")
+            self.QBIT_USER = config_load_service("QBIT_USER")
+            self.QBIT_PASS = config_load_service("QBIT_PASS")
+            self.QBIT_URL = config_load_service("QBIT_URL")
+            self.QBIT_PORT = config_load_service("QBIT_PORT")
+        except decouple.UndefinedValueError as e:
+            console.log(f"* service.env * {e}", style="red bold")
+            exit(1)
 
     def validate(self):
 
-        env_files = [os.path.splitext(file_name)[0].lower() for file_name in os.listdir() if
-                     os.path.isfile(file_name) and os.path.splitext(file_name)[1].lower() == '.env'
-                     and 'service.env' not in file_name.lower()]
+        env_files = [
+            os.path.splitext(file_name)[0].lower()
+            for file_name in os.listdir()
+            if os.path.isfile(file_name)
+            and os.path.splitext(file_name)[1].lower() == ".env"
+            and "service.env" not in file_name.lower()
+        ]
 
         for tracker_name in env_files:
             tracker_env_name = f"{tracker_name}.env"
@@ -110,14 +120,23 @@ class ConfigUnit3D:
 
             # // check tracker file configuration .env e .json
             config_load_tracker = Config(RepositoryEnv(tracker_env_path))
-            self.API_TOKEN = config_load_tracker("API_TOKEN")
-            self.BASE_URL = config_load_tracker("BASE_URL")
+            try:
+                self.API_TOKEN = config_load_tracker("API_TOKEN")
+                self.BASE_URL = config_load_tracker("BASE_URL")
+            except decouple.UndefinedValueError as e:
+                console.log(f"* {tracker_name}.env * {e}", style="red bold")
+                exit(1)
+
             tracker_values = TrackerConfig(tracker_json_path)
 
             self.trackers = TrackerManager(tracker_name)
-            self.trackers.add_tracker(Tracker(api_token=self.API_TOKEN,
-                                              base_url=self.BASE_URL,
-                                              tracker_values=tracker_values))
+            self.trackers.add_tracker(
+                Tracker(
+                    api_token=self.API_TOKEN,
+                    base_url=self.BASE_URL,
+                    tracker_values=tracker_values,
+                )
+            )
 
 
 config = ConfigUnit3D()
