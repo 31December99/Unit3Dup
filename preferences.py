a38523a60d588730c296bbb7645776554de7ed54
Parzival <jw79985@gmail.com>
2024-08-11 23:24:04 +0200

    Add an file for user preferences

---
+++ preferences.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# duplicate_on = True -> It Searches for duplicates
+# duplicate_on = False -> Does not search for duplicates
+duplicate_on = False
+
+
+# Choose the numerbers of screenshots you want upload to tracker
+number_of_screenshots = 4
