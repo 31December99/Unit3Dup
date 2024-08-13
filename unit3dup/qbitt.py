c6b11625fc160f130cca1f0e697fa4f567fc0ab8
Parzival <jw79985@gmail.com>
2024-08-14 01:04:41 +0200

    Move .torrent file if torrent_archive is set

--- unit3dup/qbitt.py
+++ unit3dup/qbitt.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import time
 import typing
 import requests
@@ -15,14 +16,14 @@ console = Console(log_path=False)
 class Qbitt:
 
     def __init__(
-        self, tracker_data_response: str, torrent: Mytorrent, contents: Contents
+            self, tracker_data_response: str, torrent: Mytorrent, contents: Contents
     ):
         self.torrent = torrent
         self.torrent_path = contents.torrent_path
         self.torrent_file = None
         self.torrents = None
+        self.torrent_archive = config.torrent_archive if os.path.exists(config.torrent_archive) else None
 
-        # c'e ancora da fare service
         self.qb = Client(f"{config.QBIT_URL}:{config.QBIT_PORT}/")
         download_torrent_dal_tracker = requests.get(tracker_data_response)
 
@@ -39,9 +40,23 @@ class Qbitt:
             self.qbit(self.torrents)
 
     def download(self, link: requests) -> typing.IO:
-        with open(f"{self.torrent_path}.torrent", "wb") as file:
+
+        # Archive the torrent file if torrent_archive is set
+        if self.torrent_archive:
+            full_path_origin = f"{self.torrent_path}.torrent"
+            file_name = f"{os.path.basename(self.torrent_path)}.torrent"
+            full_path_archive = os.path.join(self.torrent_archive, file_name)
+            os.replace(full_path_origin, full_path_archive)
+        else:
+            # Or save to current the path
+            full_path_archive = f"{self.torrent_path}.torrent"
+
+        # File archived
+        with open(full_path_archive, "wb") as file:
             file.write(link.content)
-        return open(f"{self.torrent_path}.torrent", "rb")
+
+        # Ready for seeding
+        return open(full_path_archive, "rb")
 
     def qbit(self, torrents: list) -> bool:
         for torrent in torrents:
@@ -51,7 +66,7 @@ class Qbitt:
                 self.qb.recheck(infohash_list=infohash)
                 console.log(f"\n[TORRENT INFOHASH]............  {infohash}")
                 console.log(
-                    f"[TORRENT LOCATION]............  {self.torrent.mytorr.location}"
+                    f"[FILES LOCATION]..............  {self.torrent.mytorr.location}"
                 )
                 console.log(
                     f"[TORRENT NAME]................  {self.torrent.mytorr.name}.torrent"
