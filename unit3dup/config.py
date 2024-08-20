a495b6f91eca3249f33b87e879d109aca3b5037c
Parzival <jw79985@gmail.com>
2024-08-20 18:50:27 +0200

    Move preferences in service.env

--- unit3dup/config.py
+++ unit3dup/config.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import ast
-
 import decouple
 from decouple import Config, RepositoryEnv
 from rich.console import Console
@@ -65,16 +63,16 @@ class ConfigUnit3D:
         self.QBIT_PORT: str = ""
         self.API_TOKEN: str = ""
         self.BASE_URL: str = ""
+        self.DUPLICATE: str = ""
+        self.SCREENSHOTS = 0
+        self.TORRENT_ARCHIVE: str = ""
+
         self.tracker_values: dict = {}
         self.trackers = None
-        self.duplicate_on = None
-        self.torrent_archive = None
-        self.number_of_screenshots = None
 
     def service(self):
 
         service_not_found: bool = False
-        preferences_not_found: bool = False
 
         # Get the current folder
         current_folder = os.path.dirname(__file__)
@@ -92,34 +90,6 @@ class ConfigUnit3D:
             )
             console.log(self.message)
 
-        preferences_path = os.path.join(root_folder, "preferences.cfg")
-
-        # Does it Exist ?
-        if not os.path.isfile(preferences_path):
-            preferences_not_found = True
-
-        if preferences_not_found:
-            self.message.append(
-                f"\nfile 'preferences.cfg' not found in {preferences_path.upper()}",
-                style="bold red",
-            )
-            console.log(self.message)
-
-        try:
-            config_load_preferences = Config(RepositoryEnv(preferences_path))
-            self.duplicate_on = ast.literal_eval(config_load_preferences("duplicate_on"))
-            self.number_of_screenshots = int(config_load_preferences("number_of_screenshots"))
-
-            torrent_archive_path = config_load_preferences("torrent_archive")
-            if torrent_archive_path != '' and not os.path.exists(torrent_archive_path):
-                console.log("The torrent archive path does not exist. Default path has been set", style="bold red")
-            else:
-                self.torrent_archive = torrent_archive_path
-
-        except decouple.UndefinedValueError as e:
-            console.log(f"* preferences.cfg * {e}", style="red bold")
-            exit(1)
-
         try:
             config_load_service = Config(RepositoryEnv(service_path))
             self.TMDB_APIKEY = config_load_service("TMDB_APIKEY")
@@ -129,6 +99,9 @@ class ConfigUnit3D:
             self.QBIT_PASS = config_load_service("QBIT_PASS")
             self.QBIT_URL = config_load_service("QBIT_URL")
             self.QBIT_PORT = config_load_service("QBIT_PORT")
+            self.DUPLICATE = config_load_service("duplicate_on")
+            self.SCREENSHOTS = int(config_load_service("number_of_screenshots"))
+            self.TORRENT_ARCHIVE = config_load_service("torrent_archive")
         except decouple.UndefinedValueError as e:
             console.log(f"* service.env * {e}", style="red bold")
             exit(1)
@@ -136,15 +109,14 @@ class ConfigUnit3D:
             pass
             exit(1)
 
-
     def validate(self):
 
         env_files = [
             os.path.splitext(file_name)[0].lower()
             for file_name in os.listdir()
             if os.path.isfile(file_name)
-               and os.path.splitext(file_name)[1].lower() == ".env"
-               and "service.env" not in file_name.lower()
+            and os.path.splitext(file_name)[1].lower() == ".env"
+            and "service.env" not in file_name.lower()
         ]
 
         for tracker_name in env_files:
@@ -153,7 +125,9 @@ class ConfigUnit3D:
 
             # Build complete paths
             tracker_env_path = os.path.join(self.root_folder, tracker_env_name)
-            tracker_json_path = os.path.join(self.root_folder, 'database', tracker_json_name)
+            tracker_json_path = os.path.join(
+                self.root_folder, "database", tracker_json_name
+            )
 
             # // check tracker file configuration .env e .json
             config_load_tracker = Config(RepositoryEnv(tracker_env_path))
