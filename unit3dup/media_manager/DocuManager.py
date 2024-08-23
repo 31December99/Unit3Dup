e2b334f809d52d4b2993d573caf1f2bfe5d0508c
Parzival <jw79985@gmail.com>
2024-08-23 20:55:08 +0200

    Remove unused import in DocuManager class

--- unit3dup/media_manager/DocuManager.py
+++ unit3dup/media_manager/DocuManager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from unit3dup.pvtTorrent import Mytorrent
-from unit3dup.uploader import UploadDocument, UploadVideo
+from unit3dup.uploader import UploadDocument
 from unit3dup.contents import Contents
 
 class DocuManager:
@@ -9,7 +9,6 @@ class DocuManager:
     def __init__(self, content: Contents):
         self.content = content
         self._my_torrent = Mytorrent(contents=content, meta=content.metainfo)
-        # self._duplicate = # not implemented #
         self._unit3d_up = UploadDocument(content)
 
     def torrent(self):
