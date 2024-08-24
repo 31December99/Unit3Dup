8043420d4b20d0bc4770ab9f447df591a9cbea0f
Parzival <jw79985@gmail.com>
2024-08-24 22:41:06 +0200

    Add a new class manager for mediainfo

---
+++ unit3dup/media_manager/MediaInfoManager.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from common.mediainfoOutput import Parser
+
+
+class MediaInfoManager:
+
+    def __init__(self, media_info_output: str):
+        self.parser = Parser(media_info=media_info_output)
+        self.audio = self.parser.get_audio_formats()
+
+    def languages(self):
+        """ return every languages present in audioFormat """
+        return [audio.language for audio in self.audio] if self.audio else []
+
+    def search_language(self, language: str) -> bool:
+        if language in self.languages():
+            return True
+        return False
