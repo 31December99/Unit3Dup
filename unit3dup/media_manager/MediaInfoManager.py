ab0dc1dfe08e4d2cc78eecc30d52822949c6cff5
Parzival <jw79985@gmail.com>
2024-08-25 03:22:39 +0200

    Add case insensitive

--- unit3dup/media_manager/MediaInfoManager.py
+++ unit3dup/media_manager/MediaInfoManager.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from common.mediainfoOutput import Parser
-
-
 class MediaInfoManager:
 
     def __init__(self, media_info_output: str):
@@ -11,9 +9,9 @@ class MediaInfoManager:
 
     def languages(self):
         """ return every languages present in audioFormat """
-        return [audio.language for audio in self.audio] if self.audio else []
+        return [audio.language.lower() for audio in self.audio] if self.audio else []
 
     def search_language(self, language: str) -> bool:
-        if language in self.languages():
+        if language.lower() in self.languages():
             return True
         return False
