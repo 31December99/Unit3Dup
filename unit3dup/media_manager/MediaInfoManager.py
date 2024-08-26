b4c42866c290c4e196dcabdd51640ec1b7a715da
Parzival <jw79985@gmail.com>
2024-08-26 22:42:45 +0200

    Replace the audio language list with a string

--- unit3dup/media_manager/MediaInfoManager.py
+++ unit3dup/media_manager/MediaInfoManager.py
@@ -11,11 +11,13 @@ class MediaInfoManager:
         if media_info_output['media_info']:
             self.parser = MediaInfo(media_info=media_info_output['media_info'])
             self.audio = self.parser.get_audio_formats()
-            self.languages = [audio.language.lower() for audio in self.audio] if self.audio else []
+            self.languages = [audio.language.lower() for audio in self.audio if audio.language] if self.audio else []
+            self.languages = ','.join(set(self.languages))
 
         if media_info_output['bd_info']:
             self.parser = BDInfo.from_bdinfo_string(media_info_output['bd_info'])
             self.languages = self.parser.languages
+            self.languages = ','.join(set(self.languages))
 
     def search_language(self, language: str) -> bool:
         return any(language.lower() in lang for lang in self.languages)
