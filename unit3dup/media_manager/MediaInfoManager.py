02c48bf97b410808878154a413c558e37023e52b
Parzival <jw79985@gmail.com>
2024-08-26 18:28:00 +0200

    Add search_language() for the bdinfo field string from the tracker

--- unit3dup/media_manager/MediaInfoManager.py
+++ unit3dup/media_manager/MediaInfoManager.py
@@ -11,17 +11,11 @@ class MediaInfoManager:
         if media_info_output['media_info']:
             self.parser = MediaInfo(media_info=media_info_output['media_info'])
             self.audio = self.parser.get_audio_formats()
+            self.languages = [audio.language.lower() for audio in self.audio] if self.audio else []
 
         if media_info_output['bd_info']:
             self.parser = BDInfo.from_bdinfo_string(media_info_output['bd_info'])
-            print(self.parser.audio)
-            input("MediaInfoManager . .-> ")
-
-    def languages(self):
-        """ return every languages present in audioFormat """
-        return [audio.language.lower() for audio in self.audio] if self.audio else []
+            self.languages = self.parser.languages
 
     def search_language(self, language: str) -> bool:
-        if language.lower() in self.languages():
-            return True
-        return False
+        return any(language.lower() in lang for lang in self.languages)
