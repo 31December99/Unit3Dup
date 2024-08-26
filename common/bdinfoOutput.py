e512ecfb32b4c16fe216391968d1dbf5c80bc236
Parzival <jw79985@gmail.com>
2024-08-26 22:38:34 +0200

    Add audio language parser

--- common/bdinfoOutput.py
+++ common/bdinfoOutput.py
@@ -2,8 +2,8 @@
 
 
 class BDInfo:
-    def __init__(self, disc_label, disc_size, protection, playlist, size, length, total_bitrate, video, languages,
-                 subtitles):
+    def __init__(self, disc_label, disc_size, protection, playlist, size, length, total_bitrate, video, audio,
+                 languages, subtitles):
         self.disc_label = disc_label
         self.disc_size = disc_size
         self.protection = protection
@@ -12,6 +12,7 @@ class BDInfo:
         self.length = length
         self.total_bitrate = total_bitrate
         self.video = video
+        self.audio = audio
         self.languages = languages
         self.subtitles = subtitles
 
@@ -24,7 +25,7 @@ class BDInfo:
         }
 
         for line in lines:
-            # Parsing
+            # Parsing Audio
             if ': ' in line:
                 key, value = line.split(': ', 1)
                 key = key.strip().replace(' ', '_').lower()
@@ -36,6 +37,15 @@ class BDInfo:
                 else:
                     data[key] = value.strip()
 
+        # Parsing Languages
+        languages_parsed = []
+        for item in data['audio']:
+            item = item.replace("(", " ")
+            item = item.replace(")", " ")
+            item = item.split('/')
+            languages_parsed.append(item[0].strip())
+        #languages = ','.join(set(languages_parsed))
+
         return cls(
             disc_label=data.get('disc_label'),
             disc_size=data.get('disc_size'),
@@ -45,7 +55,7 @@ class BDInfo:
             length=data.get('length'),
             total_bitrate=data.get('total_bitrate'),
             video=data.get('video'),
-            languages=data['audio'],
+            audio=data['audio'],
+            languages=languages_parsed,
             subtitles=data['subtitles']
         )
-
