16cfebc387563189d77357c89a76ae9c4782bfa1
Parzival <jw79985@gmail.com>
2024-09-01 02:47:29 +0200

    Add a table to view the results from tmdb

--- media_db/search.py
+++ media_db/search.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 import sys
 
-from common.utility import title
 from media_db import tmdb
+from common.utility import title
 from common.custom_console import custom_console
+from unit3dup.contents import Contents
 
 
 class TvShow:
 
-    def __init__(self, category: int):
+    def __init__(self, content: Contents):
         super().__init__()
         self.titles = None
+        self.content = content
+        category = content.category
+
         show = {
             1: "Movie",
             2: "Serie",
@@ -39,15 +43,26 @@ class TvShow:
                 if backdrop_path
                 else "nourl"
             )
+            result.backdrop_path = url_backdrop
+
             url_poster = (
                 f"https://www.themoviedb.org/t/p/original{poster_path}"
                 if poster_path
                 else "nourl"
             )
+            result.poster_path = url_poster
+            """
             custom_console.bot_log(f"\n[TMDB ID]................  {result.video_id}")
-            custom_console.bot_log(f"[TMDB POSTER]............  {url_poster}")
-            custom_console.bot_log(f"[TMDB BACKDROP]..........  {url_backdrop}")
+            custom_console.bot_log(f"[TMDB POSTER]............  {result.poster_path}")
+            custom_console.bot_log(f"[TMDB BACKDROP]..........  {result.backdrop_path}")
             custom_console.bot_log(f"[TMDB KEYWORDS]..........  {result.keywords}\n")
+            """
+
+            # Print the list of search results
+            custom_console.bot_tmdb_table_log(
+                result=result, title=_title, media_info_language=self.content.audio_languages
+            )
+
             return result
         else:
             custom_console.bot_log(f"Non trovo un ID valido per {file_name}")
