822dac9302d594ce01b9a624daec6db6348e5a7a
Parzival <jw79985@gmail.com>
2024-09-04 01:38:57 +0200

    Add basic class for tvshow/movie tmdb

--- common/external_services/sources_manager/JackManager.py
+++ common/external_services/sources_manager/JackManager.py
@@ -1,66 +1,57 @@
 # -*- coding: utf-8 -*-
 
-import requests
-from common.config import config
-from common.external_services.jack import NowPlaying, LatestRelease
-from common.custom_console import custom_console
+from common.external_services.theMovieDB.country import TvShowByCountry
+from common.external_services.theMovieDB.tmdb import Tmdb
+from common.external_services.theMovieDB.response import NowPlayingByCountry
 
 
 class JackManager:
 
-    def search_jack(self, query):
-        params = {
-            "apikey": config.JACK_API_KEY,
-            "Query": query,
-            "Category[]": [2000],
-        }
+    def __init__(self):
+        self.tmdb = Tmdb()
 
-        try:
-            response = requests.get(config.JACK_URL, params=params)
-            response.raise_for_status()
+    # The latest in country
+    def latest_movie_by_country(self, country_code: str) -> list:
+        """
+        Retrieve the latest movies currently playing in a specific country.
 
-            results = response.json().get("Results", [])
-            for result in results:
-                title = result["Title"]
-                # Controlla se il torrent è in italiano
-                if self.is_italian(title):
-                    print(f"[Jack] Title: {title}")
-                    # print(f"Link: {result['Link']}")
-                    # print(f"Seeders: {result['Seeders']}")
-                    # print(f"Peers: {result['Peers']}")
-                    print("-" * 40)
+        Fetches movies that are currently playing, retrieves their latest release information,
+        and filters by the given country code. Returns a list of `MovieByCountry` instances.
 
-        except requests.exceptions.RequestException as e:
-            print(f"Errore nella richiesta: {e}")
+        Parameters:
+            country_code (str): ISO 3166-1  country code (e.g., 'IT').
 
-    def is_italian(self, title):
-        italian_keywords = [
-            "ITA",
-            "Italian",
-            "Italiano",
-            "[ITA]",
-            "Sub Ita",
-            "Ita Subs",
+        Returns:
+            list: List of `MovieByCountry` instances for the specified country.
+        """
+
+        # Get every the movie now_playing
+        now_playing = self.tmdb.get_now_playing()
+        for movie in now_playing:
+
+            # For each movie get each latest
+            latest_movie_list = self.tmdb.get_latest_movie(now_playing=movie)
+
+            # For each latest search for the preferred country code
+            return [
+                NowPlayingByCountry.create(now_playing=movie, release_info=release_info)
+                for release_info in latest_movie_list
+                if release_info.iso_3166_1 == country_code
+            ]
+
+    def latest_show_by_country(self, country_code: str) -> list:
+        """
+        Retrieve the latest TV shows from a specific country
+
+        Parameters:
+            country_code (str): The ISO 3166-1 alpha-2 country code (e.g., 'IT' for Italy)
+
+        Returns:
+            list: A list of TvShowByCountry instances for TV shows from the specified country
+        """
+        # Get each tv_show by country
+        return [
+            TvShowByCountry.create(tvshow=tv_show)
+            for tv_show in self.tmdb.get_tv_show()
+            if tv_show.origin_country == country_code
         ]
-        return any(keyword.lower() in title.lower() for keyword in italian_keywords)
-
-    def now_playing(self):
-        now_playing_list = NowPlaying.get_now_playing()
-
-        for media in now_playing_list:
-            latest_release = LatestRelease.get_latest(media.movie_id)
-            release = latest_release.release_date()
-
-            # if you preferred language is found
-            if release:
-                custom_console.print(
-                    f"[green]MovieID[/green] {media.movie_id} [green]Title[/green] {media.title}"
-                    f" [green]ReleaseDate[/green] {media.release_date}"
-                    f" [green]Language[/green] {media.language}"
-                )
-
-                # TODO: search for media in the tracker beforehand
-
-                # Search in Jack
-                self.search_jack(media.title)
-                input("Push a button to continue")
