8195f50c50fe61c4fce15f3ddd3e864870f13945
Parzival <jw79985@gmail.com>
2024-09-05 01:52:20 +0200

    Add a dataclass decorator to on_air_class

--- common/external_services/theMovieDB/core/models/tvshow_on_the_air.py
+++ common/external_services/theMovieDB/core/models/tvshow_on_the_air.py
@@ -1,52 +1,30 @@
 # -*- coding: utf-8 -*-
-class OnTheAir:
-    """
-    Represents a TV show with various attributes.
-    """
+from dataclasses import dataclass
 
-    def __init__(
-            self,
-            adult=None,
-            backdrop_path=None,
-            genre_ids=None,
-            id=None,
-            original_language=None,
-            original_title=None,
-            overview=None,
-            popularity=None,
-            poster_path=None,
-            release_date=None,
-            title=None,
-            video=None,
-            vote_average=None,
-            vote_count=None,
-            origin_country=None,
-            original_name=None,
-            first_air_date=None,
-            name=None,
-    ):
-        """
-        Initializes a TvShow instance with provided attributes.
-        """
-        self.adult = adult
-        self.backdrop_path = backdrop_path
-        self.genre_ids = genre_ids
-        self.id = id
-        self.original_language = original_language
-        self.original_title = original_title
-        self.overview = overview
-        self.popularity = popularity
-        self.poster_path = poster_path
-        self.release_date = release_date
-        self.title = title
-        self.video = video
-        self.vote_average = vote_average
-        self.vote_count = vote_count
-        self.origin_country = origin_country
-        self.original_name = original_name
-        self.first_air_date = first_air_date
-        self.name = name
+
+# 05/09/2024
+# Automatically creates __init__, __repr__, and other methods
+#
+@dataclass(frozen=True)
+class OnTheAir:
+    adult: bool | None = None
+    backdrop_path: str | None = None
+    genre_ids: list[int] | None = None
+    id: int | None = None
+    original_language: str | None = None
+    original_title: str | None = None
+    overview: str | None = None
+    popularity: float | None = None
+    poster_path: str | None = None
+    release_date: str | None = None
+    title: str | None = None
+    video: bool | None = None
+    vote_average: float | None = None
+    vote_count: int | None = None
+    origin_country: list[str] | None = None
+    original_name: str | None = None
+    first_air_date: str | None = None
+    name: str | None = None
 
     def __repr__(self):
-        """Returns a string representation"""
         return f"<OnTheAir title={self.name} id={self.id}>"
