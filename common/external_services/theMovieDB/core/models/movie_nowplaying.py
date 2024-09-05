c6f30f65aaa3cc6a03c2bae629edb14204c8ce35
Parzival <jw79985@gmail.com>
2024-09-05 22:56:04 +0200

    Add the @dataclass decorator. Removed classmethods

--- common/external_services/theMovieDB/core/models/movie_nowplaying.py
+++ common/external_services/theMovieDB/core/models/movie_nowplaying.py
@@ -1,104 +1,57 @@
 # -*- coding: utf-8 -*-
 
+from dataclasses import dataclass, field
+from .movie_release_info import MovieReleaseInfo
 
+
+@dataclass
 class NowPlaying:
     """
     Represents a movie with various attributes.
     """
 
-    def __init__(
-            self,
-            adult=None,
-            backdrop_path=None,
-            genre_ids=None,
-            id=None,
-            original_language=None,
-            original_title=None,
-            overview=None,
-            popularity=None,
-            poster_path=None,
-            release_date=None,
-            title=None,
-            video=None,
-            vote_average=None,
-            vote_count=None,
-    ):
-        """
-        Initializes a Movie instance with provided attributes.
-        """
-        self.adult = adult
-        self.backdrop_path = backdrop_path
-        self.genre_ids = genre_ids
-        self.id = id
-        self.original_language = original_language
-        self.original_title = original_title
-        self.overview = overview
-        self.popularity = popularity
-        self.poster_path = poster_path
-        self.release_date = release_date
-        self.title = title
-        self.video = video
-        self.vote_average = vote_average
-        self.vote_count = vote_count
+    adult: bool | None = None
+    backdrop_path: str | None = None
+    genre_ids: list[int] = field(default_factory=list)
+    id: int | None = None
+    original_language: str | None = None
+    original_title: str | None = None
+    overview: str | None = None
+    popularity: float | None = None
+    poster_path: str | None = None
+    release_date: str | None = None
+    title: str | None = None
+    video: bool | None = None
+    vote_average: float | None = None
+    vote_count: int | None = None
 
     def __repr__(self):
         """Returns a string representation"""
         return f"<Movie title={self.title} id={self.id}>"
 
-    @classmethod
-    def release_info(cls, iso_3166_1=None, release_dates=None):
-        """
-        Initializes a ReleaseInfo instance with provided attributes.
-        """
-        return cls(iso_3166_1, release_dates)
 
-
+@dataclass
 class NowPlayingByCountry:
     """
     Represents a combined movie object with attributes from both NowPlaying and MovieReleaseInfo.
     """
 
-    def __init__(
-            self,
-            adult=None,
-            backdrop_path=None,
-            genre_ids=None,
-            id=None,
-            original_language=None,
-            original_title=None,
-            overview=None,
-            popularity=None,
-            poster_path=None,
-            release_date=None,
-            title=None,
-            video=None,
-            vote_average=None,
-            vote_count=None,
-            iso_3166_1=None,
-            release_dates=None,
-    ):
-        """
-        Initializes a CombinedMovieInfo instance with attributes from both NowPlaying and MovieReleaseInfo.
-        """
-        # Attributes from NowPlaying
-        self.adult = adult
-        self.backdrop_path = backdrop_path
-        self.genre_ids = genre_ids
-        self.id = id
-        self.original_language = original_language
-        self.original_title = original_title
-        self.overview = overview
-        self.popularity = popularity
-        self.poster_path = poster_path
-        self.release_date = release_date
-        self.title = title
-        self.video = video
-        self.vote_average = vote_average
-        self.vote_count = vote_count
-
-        # Attributes from MovieReleaseInfo
-        self.iso_3166_1 = iso_3166_1
-        self.release_dates = release_dates
+    adult: bool | None = None
+    backdrop_path: str | None = None
+    genre_ids: list[int] = field(default_factory=list)
+    id: int | None = None
+    original_language: str | None = None
+    original_title: str | None = None
+    overview: str | None = None
+    popularity: float | None = None
+    poster_path: str | None = None
+    release_date: str | None = None
+    title: str | None = None
+    video: bool | None = None
+    vote_average: float | None = None
+    vote_count: int | None = None
+    iso_3166_1: str | None = None
+    release_dates: dict[str, str] = field(default_factory=dict)
 
     def __repr__(self):
         """Returns a string representation"""
@@ -107,19 +60,21 @@ class NowPlayingByCountry:
             f"iso_3166_1={self.iso_3166_1}, release_dates={self.release_dates}>"
         )
 
-    @classmethod
-    def create(cls, now_playing, release_info):
+    @staticmethod
+    def create(
+            now_playing: NowPlaying, release_info: "MovieReleaseInfo"
+    ) -> "NowPlayingByCountry":
         """
-        Creates a CombinedMovieInfo instance from NowPlaying and MovieReleaseInfo instances.
+        Creates a NowPlayingByCountry instance from NowPlaying and MovieReleaseInfo instances.
 
         Parameters:
             now_playing (NowPlaying): An instance of the NowPlaying class.
             release_info (MovieReleaseInfo): An instance of the MovieReleaseInfo class.
 
         Returns:
-            CombinedMovieInfo: A new instance combining attributes from both classes.
+            NowPlayingByCountry: A new instance combining attributes from both classes.
         """
-        return cls(
+        return NowPlayingByCountry(
             adult=now_playing.adult,
             backdrop_path=now_playing.backdrop_path,
             genre_ids=now_playing.genre_ids,
