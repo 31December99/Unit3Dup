a0845c5aedaa818b25301bf0ad5bd842b57dfa3f
Parzival <jw79985@gmail.com>
2024-09-05 22:56:15 +0200

    Add the @dataclass decorator. Removed classmethods

--- common/external_services/theMovieDB/core/models/movie_release_info.py
+++ common/external_services/theMovieDB/core/models/movie_release_info.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 
 
+from dataclasses import dataclass, field
+
+
+@dataclass
 class MovieReleaseInfo:
     """
     Represents release information for a movie in a specific country.
     """
-
-    def __init__(self, iso_3166_1=None, release_dates=None):
-        """
-        Initializes a ReleaseInfo instance with provided attributes.
-        """
-        self.iso_3166_1 = iso_3166_1
-        self.release_dates = release_dates
+    iso_3166_1: str | None = None
+    release_dates: dict[str, str] | None = field(default_factory=dict)
 
     def __repr__(self):
         """Returns a string representation"""
         return f"<ReleaseInfo iso_3166_1={self.iso_3166_1}, release_dates={self.release_dates}>"
-
