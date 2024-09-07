a3d51e301b134cbf5232757262e1c4639c3548de
Parzival <jw79985@gmail.com>
2024-09-07 16:42:56 +0200

    Add a basic validation to nowPlaying dataclass

--- common/external_services/theMovieDB/core/models/movie/nowplaying.py
+++ common/external_services/theMovieDB/core/models/movie/nowplaying.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from dataclasses import dataclass, field
 from .release_info import MovieReleaseInfo
+from dataclasses import dataclass, field
+from common.external_services import logger
 
 
 @dataclass
 class NowPlaying:
     """
-    Represents a movie with various attributes.
+    Represents Nowplaying attributes
     """
-
     adult: bool | None = None
     backdrop_path: str | None = None
     genre_ids: list[int] = field(default_factory=list)
@@ -26,53 +26,35 @@ class NowPlaying:
     vote_count: int | None = None
 
     def __repr__(self):
-        """Returns a string representation"""
+        """Returns a string """
         return f"<Movie title={self.title} id={self.id}>"
 
 
 @dataclass
-class NowPlayingByCountry:
+class NowPlayingByCountry(NowPlaying):
     """
-    Represents a combined movie object with attributes from both NowPlaying and MovieReleaseInfo.
+    Represents a combined movie object NowPlayIng by Country code
     """
-
-    adult: bool | None = None
-    backdrop_path: str | None = None
-    genre_ids: list[int] = field(default_factory=list)
-    id: int | None = None
-    original_language: str | None = None
-    original_title: str | None = None
-    overview: str | None = None
-    popularity: float | None = None
-    poster_path: str | None = None
-    release_date: str | None = None
-    title: str | None = None
-    video: bool | None = None
-    vote_average: float | None = None
-    vote_count: int | None = None
     iso_3166_1: str | None = None
-    release_dates: list[dict[str, str]] = field(default_factory=dict)
+    release_dates: list[dict[str, str]] = field(default_factory=list)
+
+    def __post_init__(self):
+        """Validate data """
+        if self.iso_3166_1 and (len(self.iso_3166_1) != 2 or not self.iso_3166_1.isalpha()):
+            logger.debug(f"Warning: Invalid iso_3166_1 code '{self.iso_3166_1}'. It must be a two-letter country code.")
+            self.iso_3166_1 = None
 
     def __repr__(self):
-        """Returns a string representation"""
+        """Returns a string """
         return (
             f"<NowPlayingByCountry title={self.title} id={self.id}, "
             f"iso_3166_1={self.iso_3166_1}, release_dates={self.release_dates}>"
         )
 
     @staticmethod
-    def create(
-            now_playing: NowPlaying, release_info: "MovieReleaseInfo"
-    ) -> "NowPlayingByCountry":
+    def from_data(now_playing: NowPlaying, release_info: "MovieReleaseInfo") -> "NowPlayingByCountry":
         """
         Creates a NowPlayingByCountry instance from NowPlaying and MovieReleaseInfo instances.
-
-        Parameters:
-            now_playing (NowPlaying): An instance of the NowPlaying class.
-            release_info (MovieReleaseInfo): An instance of the MovieReleaseInfo class.
-
-        Returns:
-            NowPlayingByCountry: A new instance combining attributes from both classes.
         """
         return NowPlayingByCountry(
             adult=now_playing.adult,
