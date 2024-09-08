4949b8fcdd22896cff5323ca885dfb658ccece58
Parzival <jw79985@gmail.com>
2024-09-09 01:42:29 +0200

    Add a method to get the client id

--- common/external_services/Pw/pw_service.py
+++ common/external_services/Pw/pw_service.py
@@ -3,6 +3,7 @@
 from common.external_services.Pw.core.pw_api import PwAPI
 from common.external_services.Pw.core.models.indexers import Indexer
 from common.external_services.Pw.core.models.search import Search
+from common.external_services.Pw.core.models.torrent_client_config import TorrentClientConfig
 
 
 class PwService:
@@ -15,3 +16,9 @@ class PwService:
 
     def search(self, query: str) -> ['Search']:
         return self.pw_api.search(query=query)
+
+    def get_torrent_client_ids(self) -> ['TorrentClientConfig']:
+        return self.pw_api.get_torrent_client_ids()
+
+    def send_torrent_to_client(self, payload):
+        return self.pw_api.send_torrent_to_client(payload)
