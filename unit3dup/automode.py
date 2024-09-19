2a5fe74db780e9f56502f7434468f21392f5e917
Parzival <jw79985@gmail.com>
2024-09-19 23:34:47 +0200

    Add replace function for the guessit

--- unit3dup/automode.py
+++ unit3dup/automode.py
@@ -102,7 +102,9 @@ class Auto:
     def create_media_path(self, subdir: str) -> Media | None:
 
         file_name, ext = os.path.splitext(subdir)
-        guess_filename = title.Guessit(file_name)
+        # wrong guessit with '-' char , example 'S01-'
+        temp_name = Manage_titles.replace(subdir)
+        guess_filename = title.Guessit(temp_name)
 
         if guess_filename.guessit_season:
             media_type = self.serie_category
@@ -130,7 +132,7 @@ class Auto:
         Returns:
             int: The depth level of the path, where depth < 1 indicates a maximum of one level of subfolders
         """
-        return path[len(self.path):].count(os.sep)
+        return path[len(self.path) :].count(os.sep)
 
     @staticmethod
     def list_video_files(manual_path: str) -> list:
