f58e50689b2c9e9bce867b836d13f79b1fe84ddb
Parzival <jw79985@gmail.com>
2024-09-19 19:13:57 +0200

    (Fix) handles exception when keywords does not exist

--- media_db/tmdb.py
+++ media_db/tmdb.py
@@ -60,13 +60,15 @@ class MyTmdb:
         return result
 
     def input_tmdb(self) -> Results:
-        custom_console.bot_log(f"[red reverse]{self.file_name}[/red reverse] Unable to identify the TMDB ID."
-                               f" Please enter the tmdb0 ID number..")
+        custom_console.bot_log(
+            f"[red reverse]{self.file_name}[/red reverse] Unable to identify the TMDB ID."
+            f" Please enter the tmdb0 ID number.."
+        )
         results = Results()
         while True:
             tmdb_id = input(f"> ")
             if not tmdb_id.isdigit():
-                custom_console.bot_err.log(
+                custom_console.bot_error_log(
                     f"I do not recognize {tmdb_id} as a number. Please try again.."
                 )
                 continue
@@ -76,12 +78,16 @@ class MyTmdb:
             if "y" == user_answ.lower():
                 # Zero = No TMDB ID
                 if tmdb_id != "0":
-                    keywords = self.keywords(int(tmdb_id))
-                    custom_console.bot_log(keywords)
-                if "The resource you requested could not be found." not in keywords:
-                    results.video_id = tmdb_id
-                    results.keywords = keywords
-                    return results
+                    details, keywords = self.keywords(int(tmdb_id))
+                    # Keywords
+                    if keywords:
+                        results.keywords = keywords
+                    # If details then return the tmdb_id
+                    if details:
+                        results.poster_path = details["poster_path"]
+                        results.backdrop_path = details["backdrop_path"]
+                        results.video_id = tmdb_id
+                        return results
 
     def __requests(self):
         self.__result = self.tmdb.search(self.ext_title)
@@ -256,13 +262,16 @@ class MyTmdb:
                             if ratio > 95:
                                 return page
 
-    def keywords(self, video_id: int) -> str:
+    def keywords(self, video_id: int) -> (str, str):
         try:
             details = self.tmdb.details(video_id)
         except tmdbv3api.exceptions.TMDbException as e:
-            return str(e)
+            custom_console.bot_error_log(f"Keywords - {video_id} - Report it {e} ")
+            exit(1)
         if "keywords" in details:
             keywords = details.keywords
             if keywords["keywords"]:
                 keywords = ",".join([key["name"] for key in keywords["keywords"]])
-                return keywords
+        else:
+            keywords = None
+        return details, keywords
