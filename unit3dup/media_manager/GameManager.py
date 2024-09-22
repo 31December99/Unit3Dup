0dcc389799f3bfecc31b5bb88f4632d7a279bd0b
Parzival <jw79985@gmail.com>
2024-09-23 00:43:39 +0200

    Add igdb to the tracker payload

--- unit3dup/media_manager/GameManager.py
+++ unit3dup/media_manager/GameManager.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+import pprint
 
+from common.external_services.igdb.igdb_service import IGdbServiceApi
+from common.external_services.igdb.core.models.game import Game
 from unit3dup.pvtTorrent import Mytorrent
 from unit3dup.upload import UploadGame
 from unit3dup.contents import Contents
@@ -11,6 +14,16 @@ class GameManager:
         self.content = content
         self._my_torrent = Mytorrent(contents=content, meta=content.metainfo)
         self._unit3d_up = UploadGame(content)
+        # IGDB service
+        self.ig_db = IGdbServiceApi()
+        self.ig_db_data: list['Game'] = []
+
+    def igdb(self):
+        response = self.ig_db.login()
+        if response:
+            print(self.content.game_crew)
+            self.ig_db_data = self.ig_db.request(self.content.game_title, platform=self.content.game_crew)
+            print(self.ig_db_data)
 
     def torrent(self):
         self._my_torrent.hash()
@@ -18,7 +31,8 @@ class GameManager:
 
     def upload(self):
         # Create a new payload
-        data = self._unit3d_up.payload()
+        #todo : choose the best match not [0]
+        data = self._unit3d_up.payload(igdb=self.ig_db_data[0])
 
         # Get a new tracker instance
         tracker = self._unit3d_up.tracker(data=data)
