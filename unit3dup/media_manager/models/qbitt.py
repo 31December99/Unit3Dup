76b88d0954b7f1e8028b3d64f0c88eca153657ef
Parzival <jw79985@gmail.com>
2024-09-24 22:15:46 +0200

    Add a dataclass to send to the torrent client

---
+++ unit3dup/media_manager/models/qbitt.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+from unit3dup.pvtTorrent import Mytorrent
+from unit3dup.contents import Contents
+from dataclasses import dataclass
+
+
+@dataclass
+class QBittorrent:
+    tracker_response: str
+    torrent_response: Mytorrent
+    content: Contents
