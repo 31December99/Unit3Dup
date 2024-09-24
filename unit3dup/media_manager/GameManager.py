dc16c2d1d2419e3d9f4a826c5c835d18747b5fcc
Parzival <jw79985@gmail.com>
2024-09-24 22:05:40 +0200

    Add the content process to the game manager class

--- unit3dup/media_manager/GameManager.py
+++ unit3dup/media_manager/GameManager.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-import pprint
+import argparse
 
-from common.external_services.igdb.igdb_service import IGdbServiceApi
+from common.external_services.igdb.core.igdb_api import IGdbServiceApi
 from common.external_services.igdb.core.models.game import Game
+from unit3dup.media_manager.models.qbitt import QBittorrent
+from common.custom_console import custom_console
 from unit3dup.pvtTorrent import Mytorrent
 from unit3dup.upload import UploadGame
 from unit3dup.contents import Contents
@@ -10,32 +12,68 @@ from unit3dup.contents import Contents
 
 class GameManager:
 
-    def __init__(self, content: Contents):
-        self.content = content
-        self._my_torrent = Mytorrent(contents=content, meta=content.metainfo)
-        self._unit3d_up = UploadGame(content)
+    def __init__(self, contents: list["Contents"], cli: argparse.Namespace):
+        self.contents = contents
+        self.cli = cli
+
         # IGDB service
-        self.ig_db = IGdbServiceApi()
-        self.ig_db_data: list['Game'] = []
+        self.ig_db_data: list["Game"] = []
 
-    def igdb(self):
-        response = self.ig_db.login()
-        if response:
-            print(self.content.game_crew)
-            self.ig_db_data = self.ig_db.request(self.content.game_title, platform=self.content.game_crew)
-            print(self.ig_db_data)
+    def process(self) -> list["QBittorrent"]:
+        custom_console.rule()
+        if IGdbServiceApi.cls_login():
+            custom_console.bot_log("IGDB Login successful!")
+            ig_dbapi = IGdbServiceApi()
+        else:
+            custom_console.bot_error_log(
+                "IGDB Login failed. Please check your credentials"
+            )
+            exit(1)
 
-    def torrent(self):
-        self._my_torrent.hash()
-        return self._my_torrent if self._my_torrent.write() else None
+        qbittorrent_list = []
+        for content in self.contents:
+            torrent_response = self.torrent(content=content)
 
-    def upload(self):
+            if not self.cli.torrent and torrent_response:
+
+                # Look for the IGDB ID only if it is after the torrent was created
+                game_data = ig_dbapi.request(
+                    title=content.game_title, platform=content.game_tags
+                )
+
+                # Upload only if it is after the torrent was created
+                tracker_response = self.upload(content=content, ig_db_data=game_data)
+            else:
+                tracker_response = None
+
+            data_for_torrent_client = QBittorrent(
+                tracker_response=tracker_response,
+                torrent_response=torrent_response,
+                content=content,
+            )
+            qbittorrent_list.append(data_for_torrent_client)
+
+        return qbittorrent_list
+
+    @staticmethod
+    def torrent(content: Contents):
+        my_torrent = Mytorrent(contents=content, meta=content.metainfo)
+        my_torrent.hash()
+        return my_torrent if my_torrent.write() else None
+
+    @staticmethod
+    def upload(content: Contents, ig_db_data: list["Game"]):
+        unit3d_up = UploadGame(content)
+
         # Create a new payload
-        #todo : choose the best match not [0]
-        data = self._unit3d_up.payload(igdb=self.ig_db_data[0])
+        # todo : choose the best match not [0]
+        if not ig_db_data:
+            custom_console.bot_error_log("IGDB ID non trovato")
+            exit(1)
+        data = unit3d_up.payload(igdb=ig_db_data[0])
 
         # Get a new tracker instance
-        tracker = self._unit3d_up.tracker(data=data)
+        tracker = unit3d_up.tracker(data=data)
 
         # Send the payload
-        return self._unit3d_up.send(tracker=tracker)
+        return unit3d_up.send(tracker=tracker)
