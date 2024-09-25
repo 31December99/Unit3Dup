42a842474c03ba7e081908943413a90c9685da68
Parzival <jw79985@gmail.com>
2024-09-25 22:07:58 +0200

    Add the interlaced attribute

--- common/mediainfo.py
+++ common/mediainfo.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 
 from pymediainfo import MediaInfo
 
@@ -14,8 +15,8 @@ class MediaFile:
     @property
     def general_track(self):
         """Returns general information"""
-        for track in self.media_info.to_data().get('tracks', []):
-            if track.get('track_type') == 'General':
+        for track in self.media_info.to_data().get("tracks", []):
+            if track.get("track_type") == "General":
                 return track
         return {}
 
@@ -24,7 +25,7 @@ class MediaFile:
         """Returns video information"""
         video_info = []
         for track in self.media_info.tracks:
-            if track.track_type == 'Video':
+            if track.track_type == "Video":
                 video_info.append(track.to_data())
         return video_info
 
@@ -33,7 +34,7 @@ class MediaFile:
         """Returns audio information"""
         audio_info = []
         for track in self.media_info.tracks:
-            if track.track_type == 'Audio':
+            if track.track_type == "Audio":
                 audio_info.append(track.to_data())
         return audio_info
 
@@ -42,23 +43,23 @@ class MediaFile:
         """Returns the codec_id of the first video track"""
         video = self.video_track
         if video:
-            return video[0].get('codec_id', 'Unknown')
-        return 'Unknown'
+            return video[0].get("codec_id", "Unknown")
+        return "Unknown"
 
     @property
     def video_width(self):
         """Returns the width of the video"""
         video = self.video_track
         if video:
-            return video[0].get('width', 'Unknown')
-        return 'Unknown'
+            return video[0].get("width", "Unknown")
+        return "Unknown"
 
     @property
     def video_height(self):
         """Returns the height of the video"""
         video = self.video_track
         if video:
-            return video[0].get('height', None)
+            return video[0].get("height", None)
         return None
 
     @property
@@ -66,7 +67,7 @@ class MediaFile:
         """Returns the scan type"""
         video = self.video_track
         if video:
-            return video[0].get('scan_type', None)
+            return video[0].get("scan_type", None)
         return None
 
     @property
@@ -74,63 +75,63 @@ class MediaFile:
         """Returns the aspect ratio of the video"""
         video = self.video_track
         if video:
-            return video[0].get('display_aspect_ratio', 'Unknown')
-        return 'Unknown'
+            return video[0].get("display_aspect_ratio", "Unknown")
+        return "Unknown"
 
     @property
     def video_frame_rate(self):
         """Returns the frame rate of the video"""
         video = self.video_track
         if video:
-            return video[0].get('frame_rate', 'Unknown')
-        return 'Unknown'
+            return video[0].get("frame_rate", "Unknown")
+        return "Unknown"
 
     @property
     def video_bit_depth(self):
         """Returns the bit depth of the video"""
         video = self.video_track
         if video:
-            return video[0].get('bit_depth', 'Unknown')
-        return 'Unknown'
+            return video[0].get("bit_depth", "Unknown")
+        return "Unknown"
 
     @property
     def audio_codec_id(self):
         """Returns the codec_id of the first audio track"""
         audio = self.audio_track
         if audio:
-            return audio[0].get('codec_id', 'Unknown')
-        return 'Unknown'
+            return audio[0].get("codec_id", "Unknown")
+        return "Unknown"
 
     @property
     def audio_bit_rate(self):
         """Returns the bit rate of the audio"""
         audio = self.audio_track
         if audio:
-            return audio[0].get('bit_rate', 'Unknown')
-        return 'Unknown'
+            return audio[0].get("bit_rate", "Unknown")
+        return "Unknown"
 
     @property
     def audio_channels(self):
         """Returns the number of audio channels"""
         audio = self.audio_track
         if audio:
-            return audio[0].get('channels', 'Unknown')
-        return 'Unknown'
+            return audio[0].get("channels", "Unknown")
+        return "Unknown"
 
     @property
     def audio_sampling_rate(self):
         """Returns the sampling rate of the audio"""
         audio = self.audio_track
         if audio:
-            return audio[0].get('sampling_rate', 'Unknown')
-        return 'Unknown'
+            return audio[0].get("sampling_rate", "Unknown")
+        return "Unknown"
 
     @property
     def subtitle_track(self):
         """Get subtitle track"""
         subtitle_info = []
         for track in self.media_info.tracks:
-            if track.track_type == 'Text':
+            if track.track_type == "Text":
                 subtitle_info.append(track.to_data())
         return subtitle_info
 
@@ -139,20 +140,30 @@ class MediaFile:
         """Get available languages from audio and subtitle tracks"""
         languages = set()
 
-        for track in self.audio_track: # + self.subtitle_track:
-            lang = track.get('language', 'Unknown')
-            if lang != 'Unknown':
+        for track in self.audio_track:  # + self.subtitle_track:
+            lang = track.get("language", "Unknown")
+            if lang != "Unknown":
                 languages.add(lang)
-        return list(languages) if len(languages) > 0 else ['not found']
+        return list(languages) if len(languages) > 0 else ["not found"]
 
     @property
     def file_size(self):
         """Get the file size"""
         general = self.general_track
         if general:
-            return general.get('file_size', 'Unknown')
-        return 'Unknown'
+            return general.get("file_size", "Unknown")
+        return "Unknown"
 
     @property
     def info(self):
         return MediaInfo.parse(self.file_path, output="STRING", full=False)
+
+    @property
+    def is_interlaced(self):
+        video = self.video_track
+        if video:
+            encoding_settings = video[0].get("encoding_settings")
+            match = re.search(r"interlaced=(\d)", encoding_settings)
+            if match:
+                return int(match.group(1))
+        return None
