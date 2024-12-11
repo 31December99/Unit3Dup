b9aab6b2d4a9599bb027ad57290d99f54d8c3f82
Parzival <jw79985@gmail.com>
2024-12-11 16:41:32 +0100

    Add Bold to the trailer message

--- unit3dup/media_manager/VideoManager.py
+++ unit3dup/media_manager/VideoManager.py
@@ -106,7 +106,7 @@ class VideoManager:
     def _video_info(self, trailer_key):
         # Add the trailer to the torrent page's description
         video_info = Video.info(self.file_name)
-        video_info.description+=f"[spoiler=** Spoiler: WATCH THE TRAILER **][center][youtube]{trailer_key}[/youtube][/center][/spoiler]"
+        video_info.description+=f"[b][spoiler=Spoiler: PLAY TRAILER][center][youtube]{trailer_key}[/youtube][/center][/spoiler][/b]"
         return video_info
 
 
