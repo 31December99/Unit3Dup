1005feb31c12f68caca5002942e59a8859838cfa
Parzival <jw79985@gmail.com>
2025-01-06 21:16:07 +0100

    Add post method to 'session'

--- common/external_services/sessions/session.py
+++ common/external_services/sessions/session.py
@@ -5,6 +5,7 @@ import httpx
 import diskcache as dc
 import logging
 from common.external_services.sessions.exceptions import exception_handler
+from common.custom_console import custom_console
 
 ENABLE_LOG = False
 logging.getLogger("httpx").setLevel(logging.CRITICAL)
@@ -85,6 +86,53 @@ class MyHttp:
             }
 
         return response
+
+    @exception_handler(log_errors=ENABLE_LOG)
+    def post(
+            self,
+            url: str,
+            params=None,
+            headers=None,
+            data=None,
+            use_cache: bool = False,
+    ) -> httpx.Response:
+
+        """
+        GET request to the specified URL
+
+        Args:
+            url (str): The URL to request
+            use_cache (bool): Whether to use cached response if available
+            params (dict): The query parameters for the request
+        Returns:
+            httpx.Response: The response object from the Post request
+        """
+        cache_key = self.create_cache_key(url, params)
+
+        if use_cache and cache_key in self.cache:
+            response_data = self.cache[cache_key]
+            response = httpx.Response(
+                status_code=response_data["status_code"],
+                headers=response_data["headers"],
+                content=response_data["content"],
+            )
+            return response
+
+        response = self.session.post(url, params=params, headers=headers, data=data)
+
+        if response.status_code==200:
+            if use_cache:
+                self.cache[cache_key] = {
+                    "status_code": response.status_code,
+                    "headers": dict(response.headers),
+                    "content": response.content,
+                }
+            return response
+        else:
+            custom_console.bot_error_log(f"{self.__class__.__name__}: {response.content}")
+            exit()
+
+
 
     def clear_cache(self):
         """Clears the HTTP response cache"""
