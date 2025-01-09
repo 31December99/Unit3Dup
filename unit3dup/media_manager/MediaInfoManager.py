fd02b0ce1b56f1847eb1dcd289b6adcc4f8fc383
Parzival <jw79985@gmail.com>
2025-01-09 23:52:37 +0100

    (Fix) languages attribute in MediaInfoManager

--- unit3dup/media_manager/MediaInfoManager.py
+++ unit3dup/media_manager/MediaInfoManager.py
@@ -9,16 +9,18 @@ class MediaInfoManager:
 
     def __init__(self, media_info_output: dict):
 
+        self.languages = 'n/a'
+
         if media_info_output['media_info']:
             self.parser = MediaInfo(media_info=media_info_output['media_info'])
             self.audio = self.parser.get_audio_formats()
-            self.languages = [audio.language.lower() for audio in self.audio if audio.language] if self.audio else []
-            self.languages = ','.join(set(self.languages))
+            audio_languages = [audio.language.lower() for audio in self.audio if audio.language] if self.audio else []
+            self.languages = ','.join(set(audio_languages)) if audio_languages else 'n/a'
 
         if media_info_output['bd_info']:
             self.parser = BDInfo.from_bdinfo_string(media_info_output['bd_info'])
-            self.languages = self.parser.languages
-            self.languages = ','.join(set(self.languages))
+            audio_languages = self.parser.languages
+            self.languages = ','.join(set(audio_languages)) if audio_languages else 'n/a'
 
     def search_language(self, language: str) -> Optional[bool]:
         return language.lower() in self.languages.lower() if self.languages else None
