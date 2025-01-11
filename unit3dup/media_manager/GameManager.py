5f71eb8f9fef93595f6137daf53b07a29b7004ff
Parzival <jw79985@gmail.com>
2025-01-11 19:31:25 +0100

    (Fix) gamemanager to work like videomanager

--- unit3dup/media_manager/GameManager.py
+++ unit3dup/media_manager/GameManager.py
@@ -2,15 +2,13 @@
 import argparse
 import os
 
-from common.external_services.igdb.client import IGDBClient
 from unit3dup.media_manager.models.qbitt import QBittorrent
-from common.custom_console import custom_console
-from unit3dup.pvtTorrent import Mytorrent
-from unit3dup.duplicate import Duplicate
 from unit3dup.contents import Contents
 from unit3dup.upload import UploadGame
-from common.config import config
 
+from common.external_services.igdb.client import IGDBClient
+from common.utility.contents import UserContent
+from common.config import config
 
 
 class GameManager:
@@ -41,7 +39,6 @@ class GameManager:
         qbittorrent_list = []
         for content in self.contents:
 
-            # Filter contents based on existing torrents or duplicates
             # Search for the game on IGDB using the content's title and platform tags
             game_data_results = self.igdb.game(content=content)
             # Skip the upload if there is no valid IGDB
@@ -49,15 +46,15 @@ class GameManager:
                 continue
 
             # Filter contents based on existing torrents or duplicates
-            if (self.cli.duplicate or config.DUPLICATE_ON) and not self.is_duplicate(content=content):
+            if (self.cli.duplicate or config.DUPLICATE_ON) and not UserContent.is_duplicate(content=content):
 
                 # Prepare the upload game data with the search results
                 unit3d_up = UploadGame(content)
                 data = unit3d_up.payload(igdb=game_data_results)
 
                 # Create the torrent file for the content
-                if not self.torrent_file_exists(content=content):
-                    torrent_response = self.torrent(content=content)
+                if not UserContent.torrent_file_exists(content=content, class_name=self.__class__.__name__):
+                    torrent_response = UserContent.torrent(content=content)
                 else:
                     torrent_response = None
 
@@ -67,68 +64,11 @@ class GameManager:
                 # Send the upload request to the tracker
                 tracker_response = unit3d_up.send(tracker=tracker)
 
-                if not self.cli.torrent:
-                    qbittorrent_list.append(
-                        QBittorrent(
-                            tracker_response=tracker_response,
-                            torrent_response=torrent_response,
-                            content=content
-                        ))
+                qbittorrent_list.append(
+                    QBittorrent(
+                        tracker_response=tracker_response,
+                        torrent_response=torrent_response,
+                        content=content
+                    ))
         return qbittorrent_list
 
-    def torrent_file_exists(self, content: Contents) -> bool:
-        """
-        Check if a torrent file for the given content already exists
-
-        Args:
-            content (Contents): The content object
-
-        Returns:
-            bool: True if the torrent file exists otherwise False
-        """
-        base_name = os.path.basename(content.torrent_path)
-
-        if config.TORRENT_ARCHIVE:
-            this_path = os.path.join(config.TORRENT_ARCHIVE, f"{base_name}.torrent")
-        else:
-            this_path = f"{content.torrent_path}.torrent"
-
-        if os.path.exists(this_path):
-            custom_console.bot_warning_log(
-                f"** {self.__class__.__name__} **: This File already exists {this_path}\n"
-            )
-            return True
-        return False
-
-    @staticmethod
-    def torrent(content: Contents) -> Mytorrent:
-        """
-        Create a torrent file for the given content
-
-        Args:
-            content (Contents): The content object meedia
-
-        Returns:
-            Mytorrent or None: Returns a Mytorrent instance if successful, otherwise None.
-        """
-        my_torrent = Mytorrent(contents=content, meta=content.metainfo)
-        my_torrent.hash()
-        return my_torrent if my_torrent.write() else None
-
-    @staticmethod
-    def is_duplicate(content: Contents) -> bool:
-        """
-        Check if the given content is a duplicate
-
-        Args:
-            content (Contents): The content objet media
-
-        Returns:
-            bool: True if the content is a duplicate otherwise False
-        """
-        duplicate = Duplicate(content=content)
-        if duplicate.process():
-            custom_console.bot_error_log(
-                f"\n*** User chose to skip '{content.display_name}' ***\n"
-            )
-            return True
