f9a990e09cc93c996276f1005cce8eba7ce38e1d
31december99 <jw79985@gmail.com>
2025-01-21 00:36:53 +0100

    Refactoring contentmanager

--- unit3dup/media_manager/ContentManager.py
+++ unit3dup/media_manager/ContentManager.py
@@ -1,47 +1,73 @@
 # -*- coding: utf-8 -*-
 
-from typing import List, Optional
-from unit3dup.files import Files
+from unit3dup.contents import Contents
+from unit3dup.contents import Media
 from unit3dup.automode import Auto
+from unit3dup.files import Files
+
+from multiprocessing import Pool
 
 
 class ContentManager:
-    def __init__(self, path: str, tracker_name: str, mode: str, force_media_type = None):
+    def __init__(self, path: str, tracker_name: str, mode: str, force_media_type=None):
+        """
+        Args:
+            path (str): The path to the media files or directories
+            tracker_name (str): The tracker name for the content
+            mode (str):  mode 'manual' or 'automatic'
+            force_media_type: if the -serie, -movie, -game si active
+        """
         self.path = path
         self.tracker_name = tracker_name
         self.mode = mode
         self.force_media_type = force_media_type
 
-    def get_files(self) -> List:
-        # Create a list of file objects (Media) based on the commands from the CLI user
-        # For each media object, add basic attributes from the create_path method of the AutoMode class
-        return self.manual(self.mode) if self.mode in ["man", "folder"] else self.auto()
+    def get_files(self) -> list['Media']:
+        """Based on selected mode"""
+        if self.mode in ["man", "folder"]:
+            # Manual call to load files from specified path
+            return self.manual(self.mode)
+        else:
+            # Automatic call to load files based on detected content..
+            return self.auto()
 
-    def manual(self, mode: str) -> List:
+    def manual(self, mode: str) -> list['Media']:
+        """Manual process """
         auto = Auto(path=self.path, mode=mode, tracker_name=self.tracker_name, force_media_type=self.force_media_type)
-        return auto.upload()
+        file_list = auto.upload()
 
-    def auto(self) -> List:
+        # Get content object for each file
+        with Pool(processes=4) as pool:
+            contents = pool.map(self.create_content_from_media, file_list)
+
+        return contents
+
+    def auto(self) -> list['Media']:
+        """Automatic process"""
         auto = Auto(path=self.path, tracker_name=self.tracker_name, force_media_type=self.force_media_type)
-        return auto.scan()
+        file_list = auto.scan()
 
-    def get_media(self, item) -> Optional:
-        # Create a Files object with basic information from the Media class
-        # and obtain the content object by adding more attributes
+        # Get content object for each file
+        with Pool(processes=4) as pool:
+            contents = pool.map(self.create_content_from_media, file_list)
+
+        return contents
+
+    def create_content_from_media(self, media: 'Media') -> Contents:
+        """Creates a `Contents` object for each media item"""
+        # Create content using the file or folder specified by the media object
         files = Files(
-            path=item.torrent_path,
+            path=media.torrent_path,
             tracker_name=self.tracker_name,
-            media_type=item.media_type,
-            game_title=item.game_title,
-            game_crew=item.crew,
-            game_tags=item.game_tags,
-            season=item.guess_season,
-            episode=item.guess_episode,
-            screen_size=item.screen_size,
+            media_type=media.media_type,
+            game_title=media.game_title,
+            game_crew=media.crew,
+            game_tags=media.game_tags,
+            season=media.guess_season,
+            episode=media.guess_episode,
+            screen_size=media.screen_size,
         )
 
-        # Create and return a content object
+        # Get content data and return if valid
         content = files.get_data()
-        if content is False:
-            return None
-        return content
+        return content if content else None
