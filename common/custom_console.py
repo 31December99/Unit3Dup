2e0a0983bd95c2f810924006c7a4e5515ce2645a
31december99 <jw79985@gmail.com>
2025-02-09 23:25:34 +0100

    Restored the user's input when the TMDb search fails

--- common/custom_console.py
+++ common/custom_console.py
@@ -7,12 +7,12 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.text import Text
 from rich.table import Table
-from common.trackers.itt import itt_data
-
 from decouple import Config, RepositoryEnv, UndefinedValueError
 
-console = Console(log_path=False)
+from common.trackers.itt import itt_data
 
+
+console = Console(log_path=False)
 
 class CustomConsole(Console):
 
@@ -104,7 +104,7 @@ class CustomConsole(Console):
         console.log(message, style=self.question_msg_color)
 
     def bot_input_log(self, message: str):
-        console.print(message, end="", style=self.normal_color)
+        console.print(f"{message} ", end="", style=self.normal_color)
 
     def bot_question_log(self, message: str):
         console.print(message, end="", style=self.question_msg_color)
@@ -175,6 +175,19 @@ class CustomConsole(Console):
         except KeyboardInterrupt:
             custom_console.bot_error_log("\nOperation cancelled.Please update your config file")
             sys.exit(0)
+
+    @staticmethod
+    def user_input(message: str)-> int:
+        custom_console.bot_input_log(message=message)
+        try:
+            while True:
+                user_tmdb_id = input()
+                if user_tmdb_id.isdigit():
+                    user_tmdb_id = int(user_tmdb_id)
+                    return user_tmdb_id if user_tmdb_id < 999999 else 0
+        except KeyboardInterrupt:
+            custom_console.bot_error_log("\nOperation cancelled. Bye !")
+            exit(1)
 
 
 # Init custom Console
