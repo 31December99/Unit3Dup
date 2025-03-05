441b5fb6433598171780a59a48600c7de4db9cd5
31december99 <jw79985@gmail.com>
2025-03-05 19:18:55 +0100

    (Fix) import in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,6 +12,7 @@ from unit3dup.media_manager.ContentManager import ContentManager
 from unit3dup.media_manager.common import UserContent
 from unit3dup.upload import UploadBot
 from unit3dup.pvtVideo import Video
+from unit3dup.bot import Bot
 
 from view import custom_console
 
