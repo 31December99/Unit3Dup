b70eecd802be259d8eeb76e20318ee12d49caaba
31december99 <jw79985@gmail.com>
2025-03-12 00:21:04 +0100

    (fix) announce url in tracker data

--- common/trackers/data.py
+++ common/trackers/data.py
@@ -9,7 +9,7 @@ trackers_api_data = {
             "url": config_settings.tracker_config.ITT_URL,
             "api_key": config_settings.tracker_config.ITT_APIKEY,
             "pass_key": config_settings.tracker_config.ITT_PID,
-            "announce": f"{config_settings.tracker_config.ITT_URL}/{config_settings.tracker_config.ITT_PID}"
+            "announce": f"{config_settings.tracker_config.ITT_URL}/announce/{config_settings.tracker_config.ITT_PID}"
         }
     ,
     'SIS':
@@ -17,7 +17,7 @@ trackers_api_data = {
             "url": config_settings.tracker_config.SIS_URL,
             "api_key": config_settings.tracker_config.SIS_APIKEY,
             "pass_key": config_settings.tracker_config.SIS_PID,
-            "announce":  f"{config_settings.tracker_config.SIS_URL}/{config_settings.tracker_config.SIS_PID}"
+            "announce":  f"{config_settings.tracker_config.SIS_URL}/announce/{config_settings.tracker_config.SIS_PID}"
         }
 
 }
