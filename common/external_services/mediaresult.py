5bd7f25b990ba9471c740300c555fe552c453505
31december99 <jw79985@gmail.com>
2025-04-01 01:11:11 +0200

    Add the season_title and year attribute to the MediaResult class

--- common/external_services/mediaresult.py
+++ common/external_services/mediaresult.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 class MediaResult:
-    def __init__(self, result=None, video_id: int = 0, imdb_id = None, trailer_key: str = None, keywords_list: str = None):
+    def __init__(self, result=None, video_id: int = 0, imdb_id = None, trailer_key: str = None,
+                 keywords_list: str = None, season_title = None):
         self.result = result
         self.trailer_key = trailer_key
         self.keywords_list = keywords_list
         self.video_id = video_id
         self.imdb_id = imdb_id
+        self.season_title = season_title
+        self.year = None
+
+        if result:
+            try:
+                self.year = datetime.strptime(result.get_date(), '%Y-%m-%d').year
+            except ValueError:
+                pass
+
+
+
 
