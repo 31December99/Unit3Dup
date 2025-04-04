88b73fd731faa00cd964e6ba3d15b08a1b96253f
31december99 <jw79985@gmail.com>
2025-04-05 00:11:30 +0200

    Add the torrent archive_path from the start()

--- start.py
+++ start.py
@@ -30,6 +30,11 @@ def main():
     # /// Initialize command line interface
     cli = CommandLine()
 
+    # Get the torrent archive path
+    if config.user_preferences.TORRENT_ARCHIVE_PATH:
+        tracker_archive = config.user_preferences.TORRENT_ARCHIVE_PATH
+    else:
+        tracker_archive = '.'
 
     # /// Load the list of the registered trackers
     if not config.tracker_config.MULTI_TRACKER:
@@ -85,7 +90,8 @@ def main():
 
     # Manual upload mode
     if cli.args.upload:
-        bot = Bot(path=cli.args.upload, cli=cli.args, trackers_name_list=tracker_name_list)
+        bot = Bot(path=cli.args.upload, cli=cli.args, trackers_name_list=tracker_name_list,
+                  torrent_archive_path=tracker_archive)
         bot.run()
 
     # Manual folder mode
@@ -95,18 +101,19 @@ def main():
             cli=cli.args,
             mode="folder",
             trackers_name_list=tracker_name_list,
+            torrent_archive_path=tracker_archive,
         )
         bot.run()
 
     # Auto mode
     if cli.args.scan and not cli.args.ftp:
-        bot = Bot(path=cli.args.scan, cli=cli.args, mode="auto", trackers_name_list=tracker_name_list)
+        bot = Bot(path=cli.args.scan, cli=cli.args, mode="auto", trackers_name_list=tracker_name_list,
+                  torrent_archive_path=tracker_archive)
         bot.run()
 
     # Watcher
     if cli.args.watcher:
         bot = Bot(path=cli.args.watcher, cli=cli.args, mode="auto", trackers_name_list=tracker_name_list)
-
         bot.watcher(duration=config.user_preferences.WATCHER_INTERVAL, watcher_path=config.user_preferences.WATCHER_PATH,
                     destination_path = config.user_preferences.WATCHER_DESTINATION_PATH)
 
