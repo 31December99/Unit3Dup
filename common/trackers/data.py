ed39a696e8ab522eab8ac471d2ca519a06601f75
31december99 <jw79985@gmail.com>
2025-04-11 00:32:23 +0200

    (fix) Add torrent 'source' attribute

--- common/trackers/data.py
+++ common/trackers/data.py
@@ -9,7 +9,8 @@ trackers_api_data = {
             "url": config_settings.tracker_config.ITT_URL,
             "api_key": config_settings.tracker_config.ITT_APIKEY,
             "pass_key": config_settings.tracker_config.ITT_PID,
-            "announce": f"{config_settings.tracker_config.ITT_URL}/announce/{config_settings.tracker_config.ITT_PID}"
+            "announce": f"{config_settings.tracker_config.ITT_URL}/announce/{config_settings.tracker_config.ITT_PID}",
+            "source": "ItaTorrents",
         }
     ,
     'SIS':
@@ -17,7 +18,8 @@ trackers_api_data = {
             "url": config_settings.tracker_config.SIS_URL,
             "api_key": config_settings.tracker_config.SIS_APIKEY,
             "pass_key": config_settings.tracker_config.SIS_PID,
-            "announce":  f"{config_settings.tracker_config.SIS_URL}/announce/{config_settings.tracker_config.SIS_PID}"
+            "announce":  f"{config_settings.tracker_config.SIS_URL}/announce/{config_settings.tracker_config.SIS_PID}",
+            "source": "ShareIsland",
         }
 
 }
