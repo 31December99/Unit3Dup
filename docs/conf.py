d1a54cdb69e2359362b2c177c7c57f5da0972052
31december99 <jw79985@gmail.com>
2025-04-13 21:21:06 +0200

    Add sphinx

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+language = 'en'
+
+project = 'Unit3D Bot'
+copyright = '2025, Parzival'
+author = 'Parzival'
+version = '0.07.9'
+release = '0.07.9'
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_rtd_theme'
+pygments_style = 'sphinx'
