d186e9d79739f5e5ec9a6f473935e70d37eb24b8
31december99 <jw79985@gmail.com>
2025-05-05 22:07:40 +0200

    (fix) add display_name to response message

--- unit3dup/upload.py
+++ unit3dup/upload.py
@@ -35,7 +35,7 @@ class UploadBot(ABC):
 
         if tracker_response.status_code == 200:
             tracker_response_body = json.loads(tracker_response.text)
-            custom_console.bot_log(f"\n[RESPONSE]-> '{self.tracker_name}'.....{self.torrent.content.file_name} "
+            custom_console.bot_log(f"\n[RESPONSE]-> '{self.tracker_name}'.....{self.torrent.content.display_name} "
                                    f"{tracker_response_body['message'].upper()}\n")
             return tracker_response_body["data"],{}
 
@@ -73,52 +73,49 @@ class UploadBot(ABC):
 
 class Show(UploadBot):
     def send(self, path: str, nfo_path = None) -> (requests, dict):
-        if self.content.category in  {'movie','tv'}:
-            self.tracker.data["name"] = self.content.display_name
-            self.tracker.data["tmdb"] = self.torrent.payload.show_id
-            self.tracker.data["imdb"] = self.torrent.payload.imdb_id if self.torrent.payload.imdb_id else 0
-            self.tracker.data["keywords"] = self.torrent.payload.show_keywords
-            self.tracker.data["category_id"] = self.tracker_data.category.get(self.content.category)
-            self.tracker.data["anonymous"] = int(config_settings.user_preferences.ANON)
-            self.tracker.data["resolution_id"] = self.tracker_data.resolution[self.content.screen_size] \
-                if self.content.screen_size else self.tracker_data.resolution[self.content.resolution]
-            self.tracker.data["mediainfo"] = self.torrent.payload.video_info.mediainfo
-            self.tracker.data["description"] = self.torrent.payload.video_info.description + self.sign
-            self.tracker.data["sd"] = self.torrent.payload.video_info.is_hd
-            self.tracker.data["type_id"] = self.tracker_data.filter_type(self.content.file_name)
-            self.tracker.data["season_number"] = self.content.guess_season
-            self.tracker.data["episode_number"] = (self.content.guess_episode if not self.content.torrent_pack else 0)
-            self.tracker.data["personal_release"] = (int(config_settings.user_preferences.PERSONAL_RELEASE)
-                                                     or int(self.cli.personal))
+        self.tracker.data["name"] = self.content.display_name
+        self.tracker.data["tmdb"] = self.torrent.payload.show_id
+        self.tracker.data["imdb"] = self.torrent.payload.imdb_id if self.torrent.payload.imdb_id else 0
+        self.tracker.data["keywords"] = self.torrent.payload.show_keywords
+        self.tracker.data["category_id"] = self.tracker_data.category.get(self.content.category)
+        self.tracker.data["anonymous"] = int(config_settings.user_preferences.ANON)
+        self.tracker.data["resolution_id"] = self.tracker_data.resolution[self.content.screen_size] \
+            if self.content.screen_size else self.tracker_data.resolution[self.content.resolution]
+        self.tracker.data["mediainfo"] = self.torrent.payload.video_info.mediainfo
+        self.tracker.data["description"] = self.torrent.payload.video_info.description + self.sign
+        self.tracker.data["sd"] = self.torrent.payload.video_info.is_hd
+        self.tracker.data["type_id"] = self.tracker_data.filter_type(self.content.file_name)
+        self.tracker.data["season_number"] = self.content.guess_season
+        self.tracker.data["episode_number"] = (self.content.guess_episode if not self.content.torrent_pack else 0)
+        self.tracker.data["personal_release"] = (int(config_settings.user_preferences.PERSONAL_RELEASE)
+                                                 or int(self.cli.personal))
         return self._send(torrent_archive=path)
 
 
 class Games(UploadBot):
     def send(self, path: str, nfo_path=None) -> (requests, dict):
-        if self.content.category in  'game':
-            igdb_platform = self.content.platform_list[0].lower() if self.content.platform_list else ''
-            self.tracker.data["name"] = self.content.display_name
-            self.tracker.data["tmdb"] = 0
-            self.tracker.data["category_id"] = self.tracker_data.category.get(self.content.category)
-            self.tracker.data["anonymous"] = int(config_settings.user_preferences.ANON)
-            self.tracker.data["description"] = self.torrent.payload.igdb.description + self.sign if self.torrent.payload.igdb else "Sorry, there is no valid IGDB"
-            self.tracker.data["type_id"] = self.tracker_data.type_id.get(igdb_platform) if igdb_platform else 1
-            self.tracker.data["igdb"] = self.torrent.payload.igdb.id if self.torrent.payload.igdb else 1,  # need zero not one ( fix tracker)
-            self.tracker.data["personal_release"] = (int(config_settings.user_preferences.PERSONAL_RELEASE)
-                                                 or int(self.cli.personal))
+        igdb_platform = self.content.platform_list[0].lower() if self.content.platform_list else ''
+        self.tracker.data["name"] = self.content.display_name
+        self.tracker.data["tmdb"] = 0
+        self.tracker.data["category_id"] = self.tracker_data.category.get(self.content.category)
+        self.tracker.data["anonymous"] = int(config_settings.user_preferences.ANON)
+        self.tracker.data["description"] = self.torrent.payload.igdb.description + self.sign if self.torrent.payload.igdb else "Sorry, there is no valid IGDB"
+        self.tracker.data["type_id"] = self.tracker_data.type_id.get(igdb_platform) if igdb_platform else 1
+        self.tracker.data["igdb"] = self.torrent.payload.igdb.id if self.torrent.payload.igdb else 1,  # need zero not one ( fix tracker)
+        self.tracker.data["personal_release"] = (int(config_settings.user_preferences.PERSONAL_RELEASE)
+                                             or int(self.cli.personal))
         return self._send(torrent_archive=path, nfo_path=nfo_path)
 
 
 class Doc(UploadBot):
     def send(self, path: str, nfo_path=None) -> (requests, dict):
-        if self.content.category in  'docu':
-            self.tracker.data["name"] = self.content.display_name
-            self.tracker.data["tmdb"] = 0
-            self.tracker.data["category_id"] = self.tracker_data.category.get(self.content.category)
-            self.tracker.data["anonymous"] = int(config_settings.user_preferences.ANON)
-            self.tracker.data["description"] = self.torrent.payload.docu_info.description + self.sign
-            self.tracker.data["type_id"] = self.tracker_data.filter_type(self.content.file_name)
-            self.tracker.data["resolution_id"] = ""
-            self.tracker.data["personal_release"] = (int(config_settings.user_preferences.PERSONAL_RELEASE)
+        self.tracker.data["name"] = self.content.display_name
+        self.tracker.data["tmdb"] = 0
+        self.tracker.data["category_id"] = self.tracker_data.category.get(self.content.category)
+        self.tracker.data["anonymous"] = int(config_settings.user_preferences.ANON)
+        self.tracker.data["description"] = self.torrent.payload.docu_info.description + self.sign
+        self.tracker.data["type_id"] = self.tracker_data.filter_type(self.content.file_name)
+        self.tracker.data["resolution_id"] = ""
+        self.tracker.data["personal_release"] = (int(config_settings.user_preferences.PERSONAL_RELEASE)
                                                      or int(self.cli.personal))
         return self._send(torrent_archive=path)
