019579683df395f03967e9e92ea3307c9416df34
parzival <jw79985@gmail.com>
2026-02-06 19:17:40 +0100

    Add 0.8.27

--- common/settings.py
+++ common/settings.py
@@ -13,7 +13,7 @@ from common.utility import ManageTitles
 from common import trackers
 
 config_file = "Unit3Dbot.json"
-version = "0.8.26"
+version = "0.8.27"
 
 if os.name == "nt":
     PW_TORRENT_ARCHIVE_PATH: Path = Path(os.getenv("LOCALAPPDATA", ".")) / "Unit3Dup_config" / "pw_torrent_archive"
