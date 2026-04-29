11a4d324ae8956c95858272ec97d26bb60294850
parzival <jw79985@gmail.com>
2026-04-29 20:23:06 +0200

    refactor command.py

--- common/command.py
+++ common/command.py
@@ -1,120 +1,162 @@
 # -*- coding: utf-8 -*-
 import os
 import argparse
+
 from common.utility import System
 from common.settings import Load
 
 
 class CommandLine:
     """
-    Class to handle user input from the command line
+    Handle command line arguments
     """
 
     def __init__(self):
 
+        # Load config
         config = Load().load_config()
 
+        # /////////////////////////
+        # Main parser
+        # /////////////////////////
         parser = argparse.ArgumentParser(
-            description="Manage torrents, uploads, and config checks."
+            description="Manage torrents, uploads, searches and config checks",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
 
-        # Config files
-        parser.add_argument(
-            "-check", "--check", action="store_true", help="Check config"
+        # /////////////////////////
+        # Groups
+        # /////////////////////////
+        cfg_group = parser.add_argument_group("Config Commands")
+        upload_group = parser.add_argument_group("Upload Commands")
+        search_group = parser.add_argument_group("Search Commands")
+        filter_group = parser.add_argument_group("Filter Options")
+        special_group = parser.add_argument_group("Special Flags")
+
+        # /////////////////////////
+        # Config Commands
+        # /////////////////////////
+        cfg_group.add_argument(
+            "-check", "--check",
+            action="store_true",
+            help="Check config files"
         )
 
-        # Main flags
-        parser.add_argument("-u", "--upload", type=str, help="Upload path")
-        parser.add_argument("-f", "--folder", type=str, help="Upload folder")
-        parser.add_argument("-scan", "--scan", type=str, help="Scan folder")
-        parser.add_argument("-b", "--buildtags", action="store_true", help="Auto build torrent name")
+        # /////////////////////////
+        # Upload Commands
+        # /////////////////////////
+        upload_group.add_argument("-u", "--upload", type=str, help="Upload path")
+        upload_group.add_argument("-f", "--folder", type=str, help="Upload folder")
+        upload_group.add_argument("-scan", "--scan", type=str, help="Scan folder")
+        upload_group.add_argument("-b", "--buildtags", action="store_true", help="Auto build title")
+        upload_group.add_argument("-reseed", "--reseed", action="store_true", help="Reseed folder")
+        upload_group.add_argument("-watcher", "--watcher", action="store_true", help="Start watcher")
+        upload_group.add_argument("-notitle", "--notitle", type=str, help="Manual title")
 
-        parser.add_argument("-reseed", "--reseed", action="store_true", help="reseed folder")
-        parser.add_argument("-watcher", "--watcher", action="store_true", help="Start watcher")
-
-        parser.add_argument("-notitle", "--notitle", type=str, help="")
-        parser.add_argument("-tracker", "--tracker", type=str, default=config.tracker_config.MULTI_TRACKER[0],
-                            help="Upload to single tracker")
-
-        parser.add_argument("-mt", "--mt", action="store_true", help="")
-        parser.add_argument('-force', nargs='?', const="movie", type=str, default=None)
-        parser.add_argument("-noseed", "--noseed", action="store_true", help="No seeding after upload")
-        parser.add_argument("-noup", "--noup", action="store_true", help="Torrent only. No upload")
-        parser.add_argument("-dup", "--duplicate", action="store_true", help="Find duplicates")
-        parser.add_argument("-personal", "--personal", action="store_true", help="Set to personal release")
-
-        parser.add_argument("-ftp", "--ftp", action="store_true", help="Connect to FTP")
-
-        # optional
-        parser.add_argument("-dmp", "--dump", action="store_true", help="Download all torrent titles")
-        parser.add_argument("-sch", "--search", type=str, help="Search for torrent")
-        parser.add_argument("-db", "--dbsave", action="store_true", help="Save the search results")
-        parser.add_argument("-i", "--info", type=str, help="Get info on torrent")
-        parser.add_argument("-up", "--uploader", type=str, help="Search by uploader")
-        parser.add_argument("-d", "--description", type=str, help="Search by description")
-        parser.add_argument("-bd", "--bdinfo", type=str, help="Show BDInfo")
-        parser.add_argument("-m", "--mediainfo", type=str, help="Show MediaInfo")
-        parser.add_argument("-st", "--startyear", type=str, help="Start year")
-        parser.add_argument("-en", "--endyear", type=str, help="End year")
-        parser.add_argument("-type", "--type", type=str, help="Filter by type")
-        parser.add_argument("-res", "--resolution", type=str, help="Filter by resolution")
-        parser.add_argument("-file", "--filename", type=str, help="Search by filename")
-        parser.add_argument("-se", "--season", type=str, help="Season number")
-        parser.add_argument("-ep", "--episode", type=str, help="Episode number")
-        parser.add_argument("-tmdb", "--tmdb_id", type=str, help="TMDB ID")
-        parser.add_argument("-imdb", "--imdb_id", type=str, help="IMDB ID")
-        parser.add_argument("-tvdb", "--tvdb_id", type=int, help="TVDB ID")
-        parser.add_argument("-mal", "--mal_id", type=str, help="MAL ID")
-        parser.add_argument("-playid", "--playlist_id", type=str, help="Playlist ID")
-        parser.add_argument("-coll", "--collection_id", type=str, help="Collection ID")
-        parser.add_argument("-free", "--freelech", type=str, help="Freelech discount")
-        parser.add_argument("-al", "--alive", action="store_true", help="Alive torrent")
-        parser.add_argument("-dd", "--dead", action="store_true", help="Dead torrent")
-        parser.add_argument("-dy", "--dying", action="store_true", help="Dying torrent")
-
-        parser.add_argument(
-            "-du", "--doubleup", action="store_true", help="DoubleUp torrent"
-        )
-        parser.add_argument(
-            "-fe", "--featured", action="store_true", help="Featured torrent"
-        )
-        parser.add_argument(
-            "-re", "--refundable", action="store_true", help="Refundable torrent"
-        )
-        parser.add_argument(
-            "-str", "--stream", action="store_true", help="Stream torrent"
-        )
-        parser.add_argument(
-            "-sd", "--standard", action="store_true", help="Standard definition torrent"
-        )
-        parser.add_argument(
-            "-hs", "--highspeed", action="store_true", help="Highspeed torrent"
-        )
-        parser.add_argument(
-            "-int", "--internal", action="store_true", help="Internal torrent"
+        upload_group.add_argument(
+            "-tracker", "--tracker",
+            type=str,
+            default=config.tracker_config.MULTI_TRACKER[0],
+            help="Single tracker"
         )
 
-        parser.add_argument(
-            "-pr", "--prelease", action="store_true", help="Personal release torrent"
+        upload_group.add_argument("-mt", "--mt", action="store_true", help="Multi tracker")
+
+        upload_group.add_argument(
+            "-force",
+            nargs='?',
+            const="movie",
+            type=str,
+            default=None,
+            help="Force category"
         )
 
-        # Parsing the User cli
-        self.args: parser = parser.parse_args()
+        upload_group.add_argument("-noseed", "--noseed", action="store_true", help="Disable seeding")
+        upload_group.add_argument("-noup", "--noup", action="store_true", help="Torrent only")
+        upload_group.add_argument("-dup", "--duplicate", action="store_true", help="Check duplicates")
+        upload_group.add_argument("-personal", "--personal", action="store_true", help="Personal release")
+        upload_group.add_argument("-ftp", "--ftp", action="store_true", help="Connect FTP")
 
-        # Test user scan path
+        # /////////////////////////
+        # Search Commands
+        # /////////////////////////
+        search_group.add_argument("-dmp", "--dump", action="store_true", help="Dump titles")
+        search_group.add_argument("-sch", "--search", type=str, help="Search torrent")
+        search_group.add_argument("-db", "--dbsave", action="store_true", help="Save results")
+        search_group.add_argument("-i", "--info", type=str, help="Torrent info")
+        search_group.add_argument("-up", "--uploader", type=str, help="By uploader")
+        search_group.add_argument("-d", "--description", type=str, help="By description")
+        search_group.add_argument("-bd", "--bdinfo", type=str, help="Show BDInfo")
+        search_group.add_argument("-m", "--mediainfo", type=str, help="Show MediaInfo")
+
+        # /////////////////////////
+        # Filter Options
+        # /////////////////////////
+        filter_group.add_argument("-st", "--startyear", type=str, help="Start year")
+        filter_group.add_argument("-en", "--endyear", type=str, help="End year")
+        filter_group.add_argument("-type", "--type", type=str, help="Type")
+        filter_group.add_argument("-res", "--resolution", type=str, help="Resolution")
+        filter_group.add_argument("-file", "--filename", type=str, help="Filename")
+        filter_group.add_argument("-se", "--season", type=str, help="Season")
+        filter_group.add_argument("-ep", "--episode", type=str, help="Episode")
+
+        # IDs
+        filter_group.add_argument("-tmdb", "--tmdb_id", type=str, help="TMDB ID")
+        filter_group.add_argument("-imdb", "--imdb_id", type=str, help="IMDB ID")
+        filter_group.add_argument("-tvdb", "--tvdb_id", type=int, help="TVDB ID")
+        filter_group.add_argument("-mal", "--mal_id", type=str, help="MAL ID")
+        filter_group.add_argument("-playid", "--playlist_id", type=str, help="Playlist ID")
+        filter_group.add_argument("-coll", "--collection_id", type=str, help="Collection ID")
+
+        # Status
+        filter_group.add_argument("-free", "--freelech", type=str, help="Freeleech")
+        filter_group.add_argument("-al", "--alive", action="store_true", help="Alive")
+        filter_group.add_argument("-dd", "--dead", action="store_true", help="Dead")
+        filter_group.add_argument("-dy", "--dying", action="store_true", help="Dying")
+
+        # /////////////////////////
+        # Special Flags
+        # /////////////////////////
+        special_group.add_argument("-du", "--doubleup", action="store_true", help="DoubleUp")
+        special_group.add_argument("-fe", "--featured", action="store_true", help="Featured")
+        special_group.add_argument("-re", "--refundable", action="store_true", help="Refundable")
+        special_group.add_argument("-str", "--stream", action="store_true", help="Stream")
+        special_group.add_argument("-sd", "--standard", action="store_true", help="SD")
+        special_group.add_argument("-hs", "--highspeed", action="store_true", help="Highspeed")
+        special_group.add_argument("-int", "--internal", action="store_true", help="Internal")
+        special_group.add_argument("-pr", "--prelease", action="store_true", help="Personal")
+
+        # /////////////////////////
+        # Parse args
+        # /////////////////////////
+        self.args = parser.parse_args()
+
+        # /////////////////////////
+        # Check scan path
+        # /////////////////////////
         self.is_dir = os.path.isdir(self.args.scan) if self.args.scan else None
 
-        # Test the upload path. Expand path home_path with tilde
+        # /////////////////////////
+        # Expand upload path
+        # /////////////////////////
         if self.args.upload:
             self.args.upload = os.path.expanduser(self.args.upload)
 
-        # Test -force flag
+        # /////////////////////////
+        # Validate force category
+        # /////////////////////////
         if self.args.force:
+
             self.args.force = self.args.force[:10]
-            if self.args.force.lower() not in [System.category_list[System.MOVIE],
-                                               System.category_list[System.GAME],
-                                               System.category_list[System.TV_SHOW],
-                                               System.category_list[System.DOCUMENTARY]]:
+
+            valid_categories = [
+                System.category_list[System.MOVIE],
+                System.category_list[System.GAME],
+                System.category_list[System.TV_SHOW],
+                System.category_list[System.DOCUMENTARY]
+            ]
+
+            if self.args.force.lower() not in valid_categories:
                 self.args.force = None
                 print("Invalid -force category")
                 exit()
