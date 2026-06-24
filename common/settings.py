5f25c8cfb195b39d571e19ca633620d7a4b0deef
parzival <jw79985@gmail.com>
2026-06-24 22:31:55 +0200

    Append category name to the shared path

--- common/settings.py
+++ common/settings.py
@@ -454,7 +454,7 @@ class Config(BaseModel):
                     section[field] = Validate.integer(value=section[field], field_name=field)
 
                 if field in ['QBIT_PASS', 'TRASM_PASS', 'RTORR_PASS', 'QBIT_USER', 'TRASM_USER', 'RTORR_USER',
-                             'TORRENT_CLIENT', 'TAG', 'CATEGORY_MOVIE','CATEGORY_TV', 'RTORR_HOST']:
+                            'TORRENT_CLIENT', 'TAG',  'RTORR_HOST']:
                     section[field] = Validate.string(value=section[field], field_name=field)
 
                 if field in ['SHARED_TRASM_PATH', 'SHARED_QBIT_PATH', 'SHARED_RTORR_PATH']:
