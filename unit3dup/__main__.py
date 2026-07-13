9dfa688c5dd78b268af8a0751f2a9b686cf67d92
parzival <jw79985@gmail.com>
2026-07-14 00:11:56 +0200

    Add decoupling for user data from cli

--- unit3dup/__main__.py
+++ unit3dup/__main__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import json
 
+from unit3dup.common.settings import Load, DEFAULT_JSON_PATH, USER_TAGS_PATH, USER_SIGN_PATH, BAN_TAGS_PATH, version
 from unit3dup.common.torrent_clients import TransmissionClient, QbittorrentClient, RTorrentClient
 from unit3dup.common.command import CommandLine
-from unit3dup.common.settings import Load, DEFAULT_JSON_PATH, USER_TAGS_PATH, USER_SIGN_PATH, BAN_TAGS_PATH, version
+from unit3dup.common.bot_config import BotConfig
 
 from unit3dup.torrent import View
 from unit3dup import pvtTracker
@@ -31,7 +32,10 @@ def main():
     print()
 
     # /// Initialize command line interface
-    cli = CommandLine()
+    user = CommandLine()
+
+    # /// Decoupling user args from the cli namespace
+    cli = BotConfig.from_namespace(user.args)
 
     # Get the torrent archive path
     if config.user_preferences.TORRENT_ARCHIVE_PATH:
@@ -45,19 +49,19 @@ def main():
         exit(1)
 
     # Check if the tracker name exists
-    if cli.args.tracker:
-        if not any(cli.args.tracker.upper() in tracker.upper() for tracker in config.tracker_config.MULTI_TRACKER):
+    if cli.tracker:
+        if not any(cli.tracker.upper() in tracker.upper() for tracker in config.tracker_config.MULTI_TRACKER):
             custom_console.bot_error_log(
-                f"Tracker '{cli.args.tracker}' not found. Please update your configuration file")
+                f"Tracker '{cli.tracker}' not found. Please update your configuration file")
             exit()
 
-        tracker = pvtTracker.Unit3d(tracker_name=cli.args.tracker)
+        tracker = pvtTracker.Unit3d(tracker_name=cli.tracker)
         if tracker.get_alive(alive=True, perPage=1):
-            custom_console.bot_log(f"Tracker -> '{cli.args.tracker.upper()}' Online")
-            tracker_name_list = [cli.args.tracker.upper()]
+            custom_console.bot_log(f"Tracker -> '{cli.tracker.upper()}' Online")
+            tracker_name_list = [cli.tracker.upper()]
 
     # Send content to the multi_tracker list
-    if cli.args.mt:
+    if cli.mt:
         tracker_name_list = config.tracker_config.MULTI_TRACKER
         for tracker_data in tracker_name_list[1:]:
             tracker = pvtTracker.Unit3d(tracker_name=tracker_data)
@@ -65,10 +69,9 @@ def main():
                 custom_console.bot_log(f"Tracker -> '{tracker_data.upper()}' Online")
 
     # Test both clients only if used
-    if cli.args.noseed is False and cli.args.noup is False or cli.args.reseed is True:
+    if cli.noseed is False and cli.noup is False or cli.reseed is True:
         # /// Test the torrent clients
-        if cli.args.scan or cli.args.upload or cli.args.folder or cli.args.watcher:
-
+        if cli.scan or cli.upload or cli.folder or cli.watcher:
             if config.torrent_client_config.TORRENT_CLIENT.lower() == "qbittorrent":
                 test_client_torrent = QbittorrentClient()
                 if not test_client_torrent.connect():
@@ -91,7 +94,7 @@ def main():
 
     # Load User Tags list
     tags_list = None
-    if cli.args.buildtags:
+    if cli.buildtags:
         try:
             with open(USER_TAGS_PATH, "r", encoding="utf-8") as f:
                 tags_list = json.load(f)
@@ -118,16 +121,16 @@ def main():
             f"Ban list file {BAN_TAGS_PATH} not found. Please update your configuration file")
 
     # Manual upload mode
-    if cli.args.upload:
-        bot = Bot(path=cli.args.upload, cli=cli.args, trackers_name_list=tracker_name_list,
+    if cli.upload:
+        bot = Bot(path=cli.upload, cli=cli, trackers_name_list=tracker_name_list,
                   torrent_archive_path=tracker_archive, tags_list=tags_list, sign_list=sign_list, ban_list=ban_list)
         bot.run()
 
     # Manual folder mode
-    if cli.args.folder:
+    if cli.folder:
         bot = Bot(
-            path=cli.args.folder,
-            cli=cli.args,
+            path=cli.folder,
+            cli=cli,
             mode="folder",
             trackers_name_list=tracker_name_list,
             torrent_archive_path=tracker_archive,
@@ -138,14 +141,14 @@ def main():
         bot.run()
 
     # Auto mode
-    if cli.args.scan and not cli.args.ftp:
-        bot = Bot(path=cli.args.scan, cli=cli.args, mode="auto", trackers_name_list=tracker_name_list,
+    if cli.scan and not cli.ftp:
+        bot = Bot(path=cli.scan, cli=cli, mode="auto", trackers_name_list=tracker_name_list,
                   torrent_archive_path=tracker_archive, tags_list=tags_list, sign_list=sign_list, ban_list=ban_list)
         bot.run()
 
     # Watcher
-    if cli.args.watcher:
-        bot = Bot(path='', cli=cli.args, mode="auto", trackers_name_list=tracker_name_list,
+    if cli.watcher:
+        bot = Bot(path='', cli=cli, mode="auto", trackers_name_list=tracker_name_list,
                   torrent_archive_path=tracker_archive)
 
         bot.watcher(duration=config.user_preferences.WATCHER_INTERVAL,
@@ -153,154 +156,154 @@ def main():
                     destination_path=config.user_preferences.WATCHER_DESTINATION_PATH)
 
     # ftp and upload
-    if cli.args.ftp:
-        bot = Bot(path='', cli=cli.args, mode="folder", trackers_name_list=tracker_name_list)
+    if cli.ftp:
+        bot = Bot(path='', cli=cli, mode="folder", trackers_name_list=tracker_name_list)
         bot.ftp()
 
     # Commands list: commands not necessary for upload but may be useful
-    if not cli.args.tracker:
+    if not cli.tracker:
         return
 
-    torrent_info = View(tracker_name=cli.args.tracker)
+    torrent_info = View(tracker_name=cli.tracker)
 
     ############################ Filter 'Combo' ##########################
-    if cli.args.tmdb_id and cli.args.resolution:
-        torrent_info.view_tmdb_res(cli.args.tmdb_id, cli.args.resolution)
+    if cli.tmdb_id and cli.resolution:
+        torrent_info.view_tmdb_res(cli.tmdb_id, cli.resolution)
         return
     #####################################################################
 
     # Search by different criteria
-    if cli.args.search:
-        torrent_info.view_search(cli.args.search, save=cli.args.dbsave)
+    if cli.search:
+        torrent_info.view_search(cli.search, save=cli.dbsave)
         return
 
     # Dump
-    if cli.args.dump:
+    if cli.dump:
         torrent_info.view_search(" ", inkey=False, save=True)
         return
 
-    if cli.args.info:
-        torrent_info.view_search(cli.args.info, info=True)
+    if cli.info:
+        torrent_info.view_search(cli.info, info=True)
         return
 
-    if cli.args.description:
-        torrent_info.view_by_description(cli.args.description)
+    if cli.description:
+        torrent_info.view_by_description(cli.description)
         return
 
-    if cli.args.bdinfo:
-        torrent_info.view_by_bdinfo(cli.args.bdinfo)
+    if cli.bdinfo:
+        torrent_info.view_by_bdinfo(cli.bdinfo)
         return
 
-    if cli.args.uploader:
-        torrent_info.view_by_uploader(cli.args.uploader, save=cli.args.dbsave)
+    if cli.uploader:
+        torrent_info.view_by_uploader(cli.uploader, save=cli.dbsave)
         return
 
-    if cli.args.startyear:
-        torrent_info.view_by_start_year(cli.args.startyear)
+    if cli.startyear:
+        torrent_info.view_by_start_year(cli.startyear)
         return
 
-    if cli.args.endyear:
-        torrent_info.view_by_end_year(cli.args.endyear)
+    if cli.endyear:
+        torrent_info.view_by_end_year(cli.endyear)
         return
 
-    if cli.args.type:
-        torrent_info.view_by_types(cli.args.type)
+    if cli.type:
+        torrent_info.view_by_types(cli.type)
         return
 
-    if cli.args.resolution:
-        torrent_info.view_by_res(cli.args.resolution)
+    if cli.resolution:
+        torrent_info.view_by_res(cli.resolution)
         return
 
-    if cli.args.filename:
-        torrent_info.view_by_filename(cli.args.filename)
+    if cli.filename:
+        torrent_info.view_by_filename(cli.filename)
         return
 
-    if cli.args.tmdb_id:
-        torrent_info.view_by_tmdb_id(cli.args.tmdb_id)
+    if cli.tmdb_id:
+        torrent_info.view_by_tmdb_id(cli.tmdb_id)
         return
 
-    if cli.args.imdb_id:
-        torrent_info.view_by_imdb_id(cli.args.imdb_id)
+    if cli.imdb_id:
+        torrent_info.view_by_imdb_id(cli.imdb_id)
         return
 
-    if cli.args.tvdb_id:
-        torrent_info.view_by_tvdb_id(cli.args.tvdb_id)
+    if cli.tvdb_id:
+        torrent_info.view_by_tvdb_id(cli.tvdb_id)
         return
 
-    if cli.args.mal_id:
-        torrent_info.view_by_mal_id(cli.args.mal_id)
+    if cli.mal_id:
+        torrent_info.view_by_mal_id(cli.mal_id)
         return
 
-    if cli.args.playlist_id:
-        torrent_info.view_by_playlist_id(cli.args.playlist_id)
+    if cli.playlist_id:
+        torrent_info.view_by_playlist_id(cli.playlist_id)
         return
 
-    if cli.args.collection_id:
-        torrent_info.view_by_collection_id(cli.args.collection_id)
+    if cli.collection_id:
+        torrent_info.view_by_collection_id(cli.collection_id)
         return
 
-    if cli.args.freelech:
-        torrent_info.view_by_freeleech(cli.args.freelech)
+    if cli.freelech:
+        torrent_info.view_by_freeleech(cli.freelech)
         return
 
-    if cli.args.season:
-        torrent_info.view_by_season(cli.args.season)
+    if cli.season:
+        torrent_info.view_by_season(cli.season)
         return
 
-    if cli.args.episode:
-        torrent_info.view_by_episode(cli.args.episode)
+    if cli.episode:
+        torrent_info.view_by_episode(cli.episode)
         return
 
-    if cli.args.mediainfo:
-        torrent_info.view_by_mediainfo(cli.args.mediainfo)
+    if cli.mediainfo:
+        torrent_info.view_by_mediainfo(cli.mediainfo)
         return
 
-    if cli.args.alive:
+    if cli.alive:
         torrent_info.view_alive()
         return
 
-    if cli.args.dead:
+    if cli.dead:
         torrent_info.view_dead()
         return
 
-    if cli.args.dying:
+    if cli.dying:
         torrent_info.view_dying()
         return
 
-    if cli.args.doubleup:
+    if cli.doubleup:
         torrent_info.view_doubleup()
         return
 
-    if cli.args.featured:
+    if cli.featured:
         torrent_info.view_featured()
         return
 
-    if cli.args.refundable:
+    if cli.refundable:
         torrent_info.view_refundable()
         return
 
-    if cli.args.stream:
+    if cli.stream:
         torrent_info.view_stream()
         return
 
-    if cli.args.standard:
+    if cli.standard:
         torrent_info.view_sd()
         return
 
-    if cli.args.highspeed:
+    if cli.highspeed:
         torrent_info.view_highspeed()
         return
 
-    if cli.args.intern_r:
+    if cli.intern_r:
         torrent_info.view_internal()
         return
 
-    if cli.args.prelease:
+    if cli.prelease:
         torrent_info.view_personal()
         return
 
     # Handle case with no arguments
-    if not cli.args:
+    if not cli:
         custom_console.print("Syntax error! Please check your commands")
         return
 
